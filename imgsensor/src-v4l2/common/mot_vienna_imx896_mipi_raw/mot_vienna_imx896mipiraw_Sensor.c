6384a9fe9c21bd1290bbc83de28c8fec305477af
chenjiao <wingt117@motorola.com>
2024-02-26 16:40:39 +0800

    vienna: imx896 sensor bringup[4/7]
    vienna:
    --imx896 sensor bringup
    
    Change-Id: If31db327332a1409cc37295d6e2bc0dfabe57a6e
    Reviewed-on: https://gerrit.mot.com/2889175
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

---
+++ imgsensor/src-v4l2/common/mot_vienna_imx896_mipi_raw/mot_vienna_imx896mipiraw_Sensor.c
@@ -0,0 +1,488 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2022 MediaTek Inc.
+
+/*****************************************************************************
+ *
+ * Filename:
+ * ---------
+ *	 imx896mipiraw_Sensor.c
+ *
+ * Project:
+ * --------
+ *	 ALPS
+ *
+ * Description:
+ * ------------
+ *	 Source code of Sensor driver
+ *
+ *
+ *------------------------------------------------------------------------------
+ * Upper this line, this part is controlled by CC/CQ. DO NOT MODIFY!!
+ *============================================================================
+ ****************************************************************************/
+#include "mot_vienna_imx896mipiraw_Sensor.h"
+
+#define IMX896_EMBEDDED_DATA_EN 0
+
+
+static void set_group_hold(void *arg, u8 en);
+static u16 get_gain2reg(u32 gain);
+static int imx896_set_test_pattern(struct subdrv_ctx *ctx, u8 *para, u32 *len);
+static int init_ctx(struct subdrv_ctx *ctx,	struct i2c_client *i2c_client, u8 i2c_write_id);
+static int vsync_notify(struct subdrv_ctx *ctx,	unsigned int sof_cnt);
+
+/* STRUCT */
+
+static struct subdrv_feature_control feature_control_list[] = {
+	{SENSOR_FEATURE_SET_TEST_PATTERN, imx896_set_test_pattern},
+};
+
+static struct mtk_mbus_frame_desc_entry frame_desc_prev[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x1000,
+			.vsize = 0x0c00,
+			.user_data_desc = VC_STAGGER_NE,
+		},
+	},
+};
+static struct mtk_mbus_frame_desc_entry frame_desc_cap[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x1000,
+			.vsize = 0x0c00,
+			.user_data_desc = VC_STAGGER_NE,
+		},
+	},
+};
+static struct mtk_mbus_frame_desc_entry frame_desc_vid[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x1000,
+			.vsize = 0x0c00,
+			.user_data_desc = VC_STAGGER_NE,
+		},
+	},
+};
+static struct mtk_mbus_frame_desc_entry frame_desc_hs_vid[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x1000,
+			.vsize = 0x0c00,
+			.user_data_desc = VC_STAGGER_NE,
+		},
+	},
+};
+static struct mtk_mbus_frame_desc_entry frame_desc_slim_vid[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x1000,
+			.vsize = 0x0c00,
+			.user_data_desc = VC_STAGGER_NE,
+		},
+	},
+};
+
+static struct subdrv_mode_struct mode_struct[] = {
+	{
+		.frame_desc = frame_desc_prev,
+		.num_entries = ARRAY_SIZE(frame_desc_prev),
+		.mode_setting_table = imx896_preview_setting,
+		.mode_setting_len = ARRAY_SIZE(imx896_preview_setting),
+		.pclk = 1360000000,
+		.linelength = 11904,
+		.framelength = 3791,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 1645710000,
+		.readout_length = 0,
+		.read_margin = 10,
+		.framelength_step = 2,
+		.coarse_integ_step = 1,
+		.multi_exposure_shutter_range[IMGSENSOR_EXPOSURE_LE].min = 8,
+		.imgsensor_winsize_info = {
+			.full_w = 8192,
+			.full_h = 6144,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 8192,
+			.h0_size = 6144,
+			.scale_w = 4096,
+			.scale_h = 3072,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 4096,
+			.h1_size = 3072,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 4096,
+			.h2_tg_size = 3072,
+		},
+
+		.ae_binning_ratio = 1000,
+		.fine_integ_line = 826,
+		.delay_frame = 1,
+		.csi_param = {
+			.cphy_settle = 73,
+		},
+		.dpc_enabled = true, /* reg 0x0b06 */
+	},
+	{
+		.frame_desc = frame_desc_cap,
+		.num_entries = ARRAY_SIZE(frame_desc_cap),
+		.mode_setting_table = imx896_capture_setting,
+		.mode_setting_len = ARRAY_SIZE(imx896_capture_setting),
+		.pclk = 1360000000,
+		.linelength = 11904,
+		.framelength = 3791,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 1645710000,
+		.readout_length = 0,
+		.read_margin = 10,
+		.framelength_step = 2,
+		.coarse_integ_step = 1,
+		.multi_exposure_shutter_range[IMGSENSOR_EXPOSURE_LE].min = 8,
+
+		.imgsensor_winsize_info = {
+			.full_w = 8192,
+			.full_h = 6144,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 8192,
+			.h0_size = 6144,
+			.scale_w = 4096,
+			.scale_h = 3072,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 4096,
+			.h1_size = 3072,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 4096,
+			.h2_tg_size = 3072,
+		},
+
+		.ae_binning_ratio = 1465,
+		.fine_integ_line = 826,
+		.delay_frame = 1,
+		.csi_param = {
+			.cphy_settle = 73,
+		},
+		.dpc_enabled = true, /* reg 0x0b06 */
+	},
+	{
+		.frame_desc = frame_desc_vid,
+		.num_entries = ARRAY_SIZE(frame_desc_vid),
+		.mode_setting_table = imx896_normal_video_setting,
+		.mode_setting_len = ARRAY_SIZE(imx896_normal_video_setting),
+		.pclk = 1360000000,
+		.linelength = 11904,
+		.framelength = 3791,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 1645710000,
+		.readout_length = 0,
+		.read_margin = 10,
+		.framelength_step = 2,
+		.coarse_integ_step = 1,
+		.multi_exposure_shutter_range[IMGSENSOR_EXPOSURE_LE].min = 8,
+
+		.imgsensor_winsize_info = {
+			.full_w = 8192,
+			.full_h = 6144,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 8192,
+			.h0_size = 6144,
+			.scale_w = 4096,
+			.scale_h = 3072,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 4096,
+			.h1_size = 3072,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 4096,
+			.h2_tg_size = 3072,
+		},
+
+		.ae_binning_ratio = 1465,
+		.fine_integ_line = 826,
+		.delay_frame = 1,
+		.csi_param = {
+			.cphy_settle = 73,
+		},
+		.dpc_enabled = true, /* reg 0x0b06 */
+	},
+	{
+		.frame_desc = frame_desc_hs_vid,
+		.num_entries = ARRAY_SIZE(frame_desc_hs_vid),
+		.mode_setting_table = imx896_hs_video_setting,
+		.mode_setting_len = ARRAY_SIZE(imx896_hs_video_setting),
+		.pclk = 1360000000,
+		.linelength = 11904,
+		.framelength = 3791,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 1645710000,
+		.readout_length = 0,
+		.read_margin = 10,
+		.framelength_step = 2,
+		.coarse_integ_step = 1,
+		.multi_exposure_shutter_range[IMGSENSOR_EXPOSURE_LE].min = 8,
+
+		.imgsensor_winsize_info = {
+			.full_w = 8192,
+			.full_h = 6144,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 8192,
+			.h0_size = 6144,
+			.scale_w = 4096,
+			.scale_h = 3072,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 4096,
+			.h1_size = 3072,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 4096,
+			.h2_tg_size = 3072,
+		},
+
+		.ae_binning_ratio = 1465,
+		.fine_integ_line = 826,
+		.delay_frame = 1,
+		.csi_param = {
+			.cphy_settle = 73,
+		},
+		.dpc_enabled = true, /* reg 0x0b06 */
+	},
+	{
+		.frame_desc = frame_desc_slim_vid,
+		.num_entries = ARRAY_SIZE(frame_desc_slim_vid),
+		.mode_setting_table = imx896_slim_video_setting,
+		.mode_setting_len = ARRAY_SIZE(imx896_slim_video_setting),
+		.pclk = 1360000000,
+		.linelength = 11904,
+		.framelength = 3791,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 1645710000,
+		.readout_length = 0,
+		.read_margin = 10,
+		.framelength_step = 2,
+		.coarse_integ_step = 1,
+		.multi_exposure_shutter_range[IMGSENSOR_EXPOSURE_LE].min = 8,
+
+		.imgsensor_winsize_info = {
+			.full_w = 8192,
+			.full_h = 6144,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 8192,
+			.h0_size = 6144,
+			.scale_w = 4096,
+			.scale_h = 3072,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 4096,
+			.h1_size = 3072,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 4096,
+			.h2_tg_size = 3072,
+		},
+
+		.ae_binning_ratio = 1465,
+		.fine_integ_line = 826,
+		.delay_frame = 1,
+		.csi_param = {
+			.cphy_settle = 73,
+		},
+		.dpc_enabled = true, /* reg 0x0b06 */
+	},
+};
+
+static struct subdrv_static_ctx static_ctx = {
+	.sensor_id = MOT_VIENNA_IMX896_SENSOR_ID,
+	.reg_addr_sensor_id = {0x0016, 0x0017},
+	.i2c_addr_table = {0x20, 0xFF},
+	.i2c_transfer_data_type = I2C_DT_ADDR_16_DATA_8,
+	.eeprom_info = 0,
+	.eeprom_num = 0,
+	.resolution = {8192, 6144},
+	.mirror = IMAGE_HV_MIRROR,
+
+	.mclk = 24,
+	.isp_driving_current = ISP_DRIVING_4MA,
+	.sensor_interface_type = SENSOR_INTERFACE_TYPE_MIPI,
+	.mipi_sensor_type = MIPI_CPHY,
+	.mipi_lane_num = SENSOR_MIPI_3_LANE,
+	.ob_pedestal = 0x40,
+
+	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_4CELL_HW_BAYER_B,
+	.ana_gain_def = BASEGAIN * 4,
+	.ana_gain_min = BASEGAIN * 1,
+	.ana_gain_max = BASEGAIN * 64,
+	.ana_gain_type = 0,
+	.ana_gain_step = 1,
+	.ana_gain_table = imx896_ana_gain_table,
+	.ana_gain_table_size = sizeof(imx896_ana_gain_table),
+	.min_gain_iso = 100,
+	.exposure_def = 0x3D0,
+	.exposure_min = 4,
+	.exposure_max = 128 * (0xFFFF - 48),
+	.exposure_step = 1,
+	.exposure_margin = 48,
+	.dig_gain_min = BASE_DGAIN * 1,
+	.dig_gain_max = BASE_DGAIN * 1,
+	.dig_gain_step = 4,
+
+	.frame_length_max = 0xFFFC,
+	.ae_effective_frame = 2,
+	.frame_time_delay_frame = 1,
+	.start_exposure_offset = 500000,
+	.g_gain2reg = get_gain2reg,
+	.s_gph = set_group_hold,
+
+	.reg_addr_stream = 0x0100,
+	.reg_addr_mirror_flip = 0x0101,
+	.reg_addr_exposure = {
+			{0x0202, 0x0203},
+			{0x0224, 0x0225},
+	},
+	.long_exposure_support = TRUE,
+	.reg_addr_exposure_lshift = 0x3128,
+	.reg_addr_ana_gain = {
+			{0x0204, 0x0205},
+			{0x0216, 0x0217},
+	},
+	.reg_addr_dig_gain = {
+			{0x020E, 0x020F},
+			{0x0218, 0x0219},
+	},
+	.reg_addr_frame_length = {0x0340, 0x0341},
+	.reg_addr_auto_extend = 0x0350,
+	.reg_addr_frame_count = 0x0005,
+	.reg_addr_fast_mode = 0x3010,
+
+	.init_setting_table = imx896_init_setting,
+	.init_setting_len = ARRAY_SIZE(imx896_init_setting),
+	.mode = mode_struct,
+	.sensor_mode_num = ARRAY_SIZE(mode_struct),
+	.list = feature_control_list,
+	.list_len = ARRAY_SIZE(feature_control_list),
+
+	.checksum_value = 0xAF3E324F,
+};
+
+static struct subdrv_ops ops = {
+	.get_id = common_get_imgsensor_id,
+	.init_ctx = init_ctx,
+	.open = common_open,
+	.get_info = common_get_info,
+	.get_resolution = common_get_resolution,
+	.control = common_control,
+	.feature_control = common_feature_control,
+	.close = common_close,
+	.get_frame_desc = common_get_frame_desc,
+	.get_temp = common_get_temp,
+	.get_csi_param = common_get_csi_param,
+	.vsync_notify = vsync_notify,
+	.update_sof_cnt = common_update_sof_cnt,
+	.parse_ebd_line = common_parse_ebd_line,
+};
+
+static struct subdrv_pw_seq_entry pw_seq[] = {
+
+	{HW_ID_MCLK, 24, 0},
+	{HW_ID_RST, 0, 0},
+	{HW_ID_AVDD2, 1800000, 1}, //AVDD1_8V
+	{HW_ID_AVDD, 2800000, 0},
+	{HW_ID_DVDD, 1100000, 1},
+	{HW_ID_MCLK_DRIVING_CURRENT, 4, 1},
+	{HW_ID_RST, 1, 2}
+};
+
+const struct subdrv_entry mot_vienna_imx896_mipi_raw_entry = {
+	.name = "mot_vienna_imx896_mipi_raw",
+	.id = MOT_VIENNA_IMX896_SENSOR_ID,
+	.pw_seq = pw_seq,
+	.pw_seq_cnt = ARRAY_SIZE(pw_seq),
+	.ops = &ops,
+};
+
+/* FUNCTION */
+
+static void set_group_hold(void *arg, u8 en)
+{
+	struct subdrv_ctx *ctx = (struct subdrv_ctx *)arg;
+
+	if (en)
+		set_i2c_buffer(ctx, 0x0104, 0x01);
+	else
+		set_i2c_buffer(ctx, 0x0104, 0x00);
+}
+
+static u16 get_gain2reg(u32 gain)
+{
+	return (16384 - (16384 * BASEGAIN) / gain);
+}
+
+static int imx896_set_test_pattern(struct subdrv_ctx *ctx, u8 *para, u32 *len)
+{
+	u32 mode = *((u32 *)para);
+
+	if (mode != ctx->test_pattern)
+		DRV_LOG(ctx, "mode(%u->%u)\n", ctx->test_pattern, mode);
+	/* 1:Solid Color 2:Color Bar 5:Black */
+	switch (mode) {
+	case 5:
+		subdrv_i2c_wr_u8(ctx, 0x020E, 0x00); /* dig_gain = 0 */
+		break;
+	default:
+		subdrv_i2c_wr_u8(ctx, 0x0601, mode);
+		break;
+	}
+
+	if ((ctx->test_pattern) && (mode != ctx->test_pattern)) {
+		if (ctx->test_pattern == 5)
+			subdrv_i2c_wr_u8(ctx, 0x020E, 0x01);
+		else if (mode == 0)
+			subdrv_i2c_wr_u8(ctx, 0x0601, 0x00); /* No pattern */
+	}
+
+	ctx->test_pattern = mode;
+	return ERROR_NONE;
+}
+
+static int init_ctx(struct subdrv_ctx *ctx,	struct i2c_client *i2c_client, u8 i2c_write_id)
+{
+	memcpy(&(ctx->s_ctx), &static_ctx, sizeof(struct subdrv_static_ctx));
+	subdrv_ctx_init(ctx);
+	ctx->i2c_client = i2c_client;
+	ctx->i2c_write_id = i2c_write_id;
+	return 0;
+}
+
+static int vsync_notify(struct subdrv_ctx *ctx,	unsigned int sof_cnt)
+{
+	DRV_LOG(ctx, "sof_cnt(%u) ctx->ref_sof_cnt(%u) ctx->fast_mode_on(%d)",
+		sof_cnt, ctx->ref_sof_cnt, ctx->fast_mode_on);
+	if (ctx->fast_mode_on && (sof_cnt > ctx->ref_sof_cnt)) {
+		ctx->fast_mode_on = FALSE;
+		ctx->ref_sof_cnt = 0;
+		DRV_LOG(ctx, "seamless_switch disabled.");
+		subdrv_i2c_wr_u8(ctx, ctx->s_ctx.reg_addr_prsh_mode, 0x00);
+		set_i2c_buffer(ctx, 0x3010, 0x00);
+		commit_i2c_buffer(ctx);
+	}
+	return 0;
+}
