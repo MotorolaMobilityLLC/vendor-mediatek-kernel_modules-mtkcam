3b179188f7b8b743b73f7037448192501b840aff
chenjiao <wingt117@motorola.com>
2024-02-26 18:47:29 +0800

    vienna: s5kjd1 sensor bringup[4/6]
    vienna:
    --s5kjd1 sensor bringup
    
    Change-Id: I98b21121af3a198759b06f53218ba0f0ab300b83
    Reviewed-on: https://gerrit.mot.com/2889350
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: <huangfx5@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    SLTApproved: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

---
+++ imgsensor/src-v4l2/common/mot_vienna_s5kjd1_mipi_raw/mot_vienna_s5kjd1mipiraw_Sensor.c
@@ -0,0 +1,605 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2019 MediaTek Inc.
+
+/*****************************************************************************
+ *
+ * Filename:
+ * ---------
+ *	 s5kjd1mipiraw_Sensor.c
+ *
+ * Project:
+ * --------
+ *	 ALPS
+ *
+ * Description:
+ * ------------
+ *	 Source code of Sensor driver
+ *
+ *
+ *------------------------------------------------------------------------------
+ * Upper this line, this part is controlled by CC/CQ. DO NOT MODIFY!!
+ *============================================================================
+ ****************************************************************************/
+#include "mot_vienna_s5kjd1mipiraw_Sensor.h"
+
+#define USING_DPHY_N_LANE 4
+
+static void set_group_hold(void *arg, u8 en);
+static u16 get_gain2reg(u32 gain);
+static int s5kjd1_set_test_pattern(struct subdrv_ctx *ctx, u8 *para, u32 *len);
+static int s5kjd1_set_test_pattern_data(struct subdrv_ctx *ctx, u8 *para, u32 *len);
+static int init_ctx(struct subdrv_ctx *ctx,	struct i2c_client *i2c_client, u8 i2c_write_id);
+static void s5kjd1_sensor_init(struct subdrv_ctx *ctx);
+static int open(struct subdrv_ctx *ctx);
+static int s5kjd1_set_ctrl_locker(struct subdrv_ctx *ctx, u32 cid, bool *is_lock);
+
+/* STRUCT */
+
+static struct subdrv_feature_control feature_control_list[] = {
+	{SENSOR_FEATURE_SET_TEST_PATTERN, s5kjd1_set_test_pattern},
+	{SENSOR_FEATURE_SET_TEST_PATTERN_DATA, s5kjd1_set_test_pattern_data},
+};
+
+/*static struct eeprom_info_struct eeprom_info[] = {
+	{
+		.header_id = 0x010B00FF,
+		.addr_header_id = 0x00000001,
+		//.i2c_write_id = 0xA0,
+
+		.xtalk_support = TRUE,
+		.xtalk_size = 2048,
+		.addr_xtalk = 0x150F,
+	},
+};*/
+
+static struct mtk_mbus_frame_desc_entry frame_desc_prev[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x0cd0,
+			.vsize = 0x099c,
+		},
+	},
+};
+/*static struct mtk_mbus_frame_desc_entry frame_desc_cap[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x19a0,
+			.vsize = 0x1338,
+		},
+	},
+};
+
+static struct mtk_mbus_frame_desc_entry frame_desc_vid[] = {
+	{
+		.bus.csi2= {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x0cd0,
+			.vsize = 0x0734,
+		},
+	},
+};
+
+static struct mtk_mbus_frame_desc_entry frame_desc_hs_vid[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x0780,
+			.vsize = 0x0438,
+		},
+	},
+};
+
+static struct mtk_mbus_frame_desc_entry frame_desc_slim_vid[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x0280,
+			.vsize = 0x01e0,
+		},
+	},
+};
+
+#if  USING_DPHY_N_LANE == 1
+static struct mtk_mbus_frame_desc_entry frame_desc_cust1[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x780,
+			.vsize = 0x438,
+		},
+	},
+};
+
+#elif USING_DPHY_N_LANE == 2
+static struct mtk_mbus_frame_desc_entry frame_desc_cust1[] = {
+	{
+		.bus.csi2 = {
+			.channel = 0,
+			.data_type = 0x2b,
+			.hsize = 0x910,
+			.vsize = 0x6d0,
+		},
+	},
+};
+#endif
+*/
+
+static struct subdrv_mode_struct mode_struct[] = {
+	{
+		.frame_desc = frame_desc_prev,
+		.num_entries = ARRAY_SIZE(frame_desc_prev),
+		.mode_setting_table = addr_data_pair_preview,
+		.mode_setting_len = ARRAY_SIZE(addr_data_pair_preview),
+		.seamless_switch_group = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_table = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_len = PARAM_UNDEFINED,
+		.hdr_mode = HDR_NONE,
+		.raw_cnt = 1,
+		.exp_cnt = 1,
+		.pclk = 1056000000,
+		.linelength = 4304,
+		.framelength = 8176,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 624000000,
+		.readout_length = 0,
+		.read_margin = 0,
+		.imgsensor_winsize_info = {
+			.full_w = 6560,
+			.full_h = 4920,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 6560,
+			.h0_size = 4920,
+			.scale_w = 3280,
+			.scale_h = 2460,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 3280,
+			.h1_size = 2460,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 3280,
+			.h2_tg_size = 2460,
+		},
+		//.pdaf_cap = FALSE,
+		.imgsensor_pd_info = PARAM_UNDEFINED,
+		.ae_binning_ratio = 1000,
+		.fine_integ_line = 0,
+		.delay_frame = 2,
+		.csi_param = {1},
+	},
+	{
+		.frame_desc = frame_desc_prev,
+		.num_entries = ARRAY_SIZE(frame_desc_prev),
+		.mode_setting_table = addr_data_pair_preview,
+		.mode_setting_len = ARRAY_SIZE(addr_data_pair_preview),
+		.seamless_switch_group = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_table = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_len = PARAM_UNDEFINED,
+		.hdr_mode = HDR_NONE,
+		.raw_cnt = 1,
+		.exp_cnt = 1,
+		.pclk = 1056000000,
+		.linelength = 4304,
+		.framelength = 8176,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 624000000,
+		.readout_length = 0,
+		.read_margin = 0,
+		.imgsensor_winsize_info = {
+			.full_w = 6560,
+			.full_h = 4920,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 6560,
+			.h0_size = 4920,
+			.scale_w = 3280,
+			.scale_h = 2460,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 3280,
+			.h1_size = 2460,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 3280,
+			.h2_tg_size = 2460,
+		},
+		//.pdaf_cap = FALSE,
+		.imgsensor_pd_info = PARAM_UNDEFINED,
+		.ae_binning_ratio = 1000,
+		.fine_integ_line = 0,
+		.delay_frame = 2,
+		.csi_param = {1},
+	},
+	{
+		.frame_desc = frame_desc_prev,
+		.num_entries = ARRAY_SIZE(frame_desc_prev),
+		.mode_setting_table = addr_data_pair_preview,
+		.mode_setting_len = ARRAY_SIZE(addr_data_pair_preview),
+		.seamless_switch_group = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_table = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_len = PARAM_UNDEFINED,
+		.hdr_mode = HDR_NONE,
+		.raw_cnt = 1,
+		.exp_cnt = 1,
+		.pclk = 1056000000,
+		.linelength = 4304,
+		.framelength = 8176,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 624000000,
+		.readout_length = 0,
+		.read_margin = 0,
+		.imgsensor_winsize_info = {
+			.full_w = 6560,
+			.full_h = 4920,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 6560,
+			.h0_size = 4920,
+			.scale_w = 3280,
+			.scale_h = 2460,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 3280,
+			.h1_size = 2460,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 3280,
+			.h2_tg_size = 2460,
+		},
+		//.pdaf_cap = FALSE,
+		.imgsensor_pd_info = PARAM_UNDEFINED,
+		.ae_binning_ratio = 1000,
+		.fine_integ_line = 0,
+		.delay_frame = 2,
+		.csi_param = {1},
+	},
+	{
+		.frame_desc = frame_desc_prev,
+		.num_entries = ARRAY_SIZE(frame_desc_prev),
+		.mode_setting_table = addr_data_pair_preview,
+		.mode_setting_len = ARRAY_SIZE(addr_data_pair_preview),
+		.seamless_switch_group = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_table = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_len = PARAM_UNDEFINED,
+		.hdr_mode = HDR_NONE,
+		.raw_cnt = 1,
+		.exp_cnt = 1,
+		.pclk = 1056000000,
+		.linelength = 4304,
+		.framelength = 8176,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 624000000,
+		.readout_length = 0,
+		.read_margin = 0,
+		.imgsensor_winsize_info = {
+			.full_w = 6560,
+			.full_h = 4920,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 6560,
+			.h0_size = 4920,
+			.scale_w = 3280,
+			.scale_h = 2460,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 3280,
+			.h1_size = 2460,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 3280,
+			.h2_tg_size = 2460,
+		},
+		//.pdaf_cap = FALSE,
+		.imgsensor_pd_info = PARAM_UNDEFINED,
+		.ae_binning_ratio = 1000,
+		.fine_integ_line = 0,
+		.delay_frame = 2,
+		.csi_param = {1},
+	},
+	{
+		.frame_desc = frame_desc_prev,
+		.num_entries = ARRAY_SIZE(frame_desc_prev),
+		.mode_setting_table = addr_data_pair_preview,
+		.mode_setting_len = ARRAY_SIZE(addr_data_pair_preview),
+		.seamless_switch_group = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_table = PARAM_UNDEFINED,
+		.seamless_switch_mode_setting_len = PARAM_UNDEFINED,
+		.hdr_mode = HDR_NONE,
+		.raw_cnt = 1,
+		.exp_cnt = 1,
+		.pclk = 1056000000,
+		.linelength = 4304,
+		.framelength = 8176,
+		.max_framerate = 300,
+		.mipi_pixel_rate = 624000000,
+		.readout_length = 0,
+		.read_margin = 0,
+		.imgsensor_winsize_info = {
+			.full_w = 6560,
+			.full_h = 4920,
+			.x0_offset = 0,
+			.y0_offset = 0,
+			.w0_size = 6560,
+			.h0_size = 4920,
+			.scale_w = 3280,
+			.scale_h = 2460,
+			.x1_offset = 0,
+			.y1_offset = 0,
+			.w1_size = 3280,
+			.h1_size = 2460,
+			.x2_tg_offset = 0,
+			.y2_tg_offset = 0,
+			.w2_tg_size = 3280,
+			.h2_tg_size = 2460,
+		},
+		//.pdaf_cap = FALSE,
+		.imgsensor_pd_info = PARAM_UNDEFINED,
+		.ae_binning_ratio = 1000,
+		.fine_integ_line = 0,
+		.delay_frame = 2,
+		.csi_param = {1},
+	},
+};
+
+static struct subdrv_static_ctx static_ctx = {
+	.sensor_id = MOT_VIENNA_S5KJD1_SENSOR_ID,
+	.reg_addr_sensor_id = {0x0000,0x0001},
+	.i2c_addr_table = {0x20, 0x5A, 0x7A, 0xFF},
+	.i2c_burst_write_support = TRUE,
+	.i2c_transfer_data_type = I2C_DT_ADDR_16_DATA_16,
+	//.eeprom_info = eeprom_info,
+	//.eeprom_num = ARRAY_SIZE(eeprom_info),
+	.resolution = {6560, 4920},
+	.mirror = IMAGE_NORMAL,
+	.mclk = 24,
+	.isp_driving_current = ISP_DRIVING_4MA,
+	.sensor_interface_type = SENSOR_INTERFACE_TYPE_MIPI,
+	.mipi_sensor_type = MIPI_OPHY_NCSI2,
+#if USING_DPHY_N_LANE == 1
+	.mipi_lane_num = SENSOR_MIPI_1_LANE,
+#elif USING_DPHY_N_LANE == 2
+	.mipi_lane_num = SENSOR_MIPI_2_LANE,
+#else
+	.mipi_lane_num = SENSOR_MIPI_4_LANE,
+#endif
+	.ob_pedestal = 0x40,
+
+	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_4CELL_BAYER_Gr,
+	.ana_gain_def = BASEGAIN * 4,
+	.ana_gain_min = BASEGAIN * 1,
+	.ana_gain_max = BASEGAIN * 16,
+	.ana_gain_type = 2,
+	.ana_gain_step = 32,
+	.ana_gain_table = s5kjd1_ana_gain_table,
+	.ana_gain_table_size = sizeof(s5kjd1_ana_gain_table),
+	.min_gain_iso = 100,
+	.exposure_def = 0x3D0,
+	.exposure_min = 4,
+	.exposure_max = 0xFFFF - 10,
+	.exposure_step = 2,
+	.exposure_margin = 10,
+
+	.frame_length_max = 0xFFFF,
+	.ae_effective_frame = 2,
+	.frame_time_delay_frame = 2,
+	.start_exposure_offset = 500000,
+#ifdef IMGSENSOR_FUSION_TEST_WORKAROUND
+	.start_exposure_offset_custom = 5500000,
+#endif
+	.pdaf_type = PDAF_SUPPORT_NA,
+	.hdr_type = HDR_SUPPORT_NA,
+	.seamless_switch_support = FALSE,
+	.temperature_support = FALSE,
+	.g_temp = PARAM_UNDEFINED,
+	.g_gain2reg = get_gain2reg,
+	.s_gph = set_group_hold,
+
+	.reg_addr_stream = 0x0100,
+	.reg_addr_mirror_flip = 0x0101,
+	.reg_addr_exposure = {{0x0202, 0x0203},},
+	.long_exposure_support = FALSE,
+	.reg_addr_exposure_lshift = PARAM_UNDEFINED,
+	.reg_addr_ana_gain = {{0x0204, 0x0205},},
+	.reg_addr_frame_length = {0x0340, 0x0341},
+	.reg_addr_temp_en = PARAM_UNDEFINED,
+	.reg_addr_temp_read = PARAM_UNDEFINED,
+	.reg_addr_auto_extend = PARAM_UNDEFINED,
+	.reg_addr_frame_count = 0x0005,
+
+	.init_setting_table = PARAM_UNDEFINED,
+	.init_setting_len = PARAM_UNDEFINED,
+	.mode = mode_struct,
+	.sensor_mode_num = ARRAY_SIZE(mode_struct),
+	.list = feature_control_list,
+	.list_len = ARRAY_SIZE(feature_control_list),
+	//.chk_s_off_sta = 1,
+	//.chk_s_off_end = 1,
+
+	.checksum_value = 0x31E3FBE2,
+
+	/* custom stream control delay timing for hw limitation (ms) */
+	//.custom_stream_ctrl_delay = 3,
+};
+
+static struct subdrv_ops ops = {
+	.get_id = common_get_imgsensor_id,
+	.init_ctx = init_ctx,
+	.open = open,
+	.get_info = common_get_info,
+	.get_resolution = common_get_resolution,
+	.control = common_control,
+	.feature_control = common_feature_control,
+	.close = common_close,
+	.get_frame_desc = common_get_frame_desc,
+	.get_csi_param = common_get_csi_param,
+	.update_sof_cnt = common_update_sof_cnt,
+	.set_ctrl_locker = s5kjd1_set_ctrl_locker,
+};
+
+static struct subdrv_pw_seq_entry pw_seq[] = {
+	{HW_ID_MCLK, 24, 0},
+	{HW_ID_RST, 0, 0},
+	{HW_ID_DVDD, 1050000, 1},
+	{HW_ID_AVDD, 2800000, 1},
+	{HW_ID_RST, 1, 5},
+
+};
+
+const struct subdrv_entry mot_vienna_s5kjd1_mipi_raw_entry = {
+	.name = "mot_vienna_s5kjd1_mipi_raw",
+	.id = MOT_VIENNA_S5KJD1_SENSOR_ID,
+	.pw_seq = pw_seq,
+	.pw_seq_cnt = ARRAY_SIZE(pw_seq),
+	.ops = &ops,
+};
+
+/* FUNCTION */
+
+static void set_group_hold(void *arg, u8 en)
+{
+	struct subdrv_ctx *ctx = (struct subdrv_ctx *)arg;
+
+	if (en)
+		set_i2c_buffer(ctx, 0x0104, 0x01);
+	else
+		set_i2c_buffer(ctx, 0x0104, 0x00);
+}
+
+static u16 get_gain2reg(u32 gain)
+{
+	return gain * 32 / BASEGAIN;
+}
+
+static int s5kjd1_set_test_pattern(struct subdrv_ctx *ctx, u8 *para, u32 *len)
+{
+	u32 mode = *((u32 *)para);
+
+	if (mode != ctx->test_pattern)
+		DRV_LOG(ctx, "mode(%u->%u)\n", ctx->test_pattern, mode);
+	/* 1:Solid Color 2:Color Bar 5:Black */
+	if (mode)
+		subdrv_i2c_wr_u16(ctx, 0x0600, mode); /*100% Color bar*/
+	else if (ctx->test_pattern)
+		subdrv_i2c_wr_u16(ctx, 0x0600, 0x0000); /*No pattern*/
+
+	ctx->test_pattern = mode;
+	return ERROR_NONE;
+}
+
+static int s5kjd1_set_test_pattern_data(struct subdrv_ctx *ctx, u8 *para, u32 *len)
+{
+	struct mtk_test_pattern_data *data = (struct mtk_test_pattern_data *)para;
+	u16 R = (data->Channel_R >> 22) & 0x3ff;
+	u16 Gr = (data->Channel_Gr >> 22) & 0x3ff;
+	u16 Gb = (data->Channel_Gb >> 22) & 0x3ff;
+	u16 B = (data->Channel_B >> 22) & 0x3ff;
+
+	subdrv_i2c_wr_u16(ctx, 0x0602, R);
+	subdrv_i2c_wr_u16(ctx, 0x0604, Gr);
+	subdrv_i2c_wr_u16(ctx, 0x0606, B);
+	subdrv_i2c_wr_u16(ctx, 0x0608, Gb);
+
+	DRV_LOG(ctx, "mode(%u) R/Gr/Gb/B = 0x%04x/0x%04x/0x%04x/0x%04x\n",
+		ctx->test_pattern, R, Gr, Gb, B);
+	return ERROR_NONE;
+}
+
+static int init_ctx(struct subdrv_ctx *ctx,	struct i2c_client *i2c_client, u8 i2c_write_id)
+{
+	memcpy(&(ctx->s_ctx), &static_ctx, sizeof(struct subdrv_static_ctx));
+	subdrv_ctx_init(ctx);
+	ctx->i2c_client = i2c_client;
+	ctx->i2c_write_id = i2c_write_id;
+
+	return 0;
+}
+
+static void s5kjd1_sensor_init(struct subdrv_ctx *ctx)
+{
+	DRV_LOG(ctx, "E\n");
+	DRV_LOG(ctx, "Dphy-4lane setting\n");
+	subdrv_i2c_wr_u16(ctx, 0x6028, 0x2000);
+	subdrv_i2c_wr_u16(ctx, 0x0000, 0x0006);
+	subdrv_i2c_wr_u16(ctx, 0x0000, 0x3841);
+	subdrv_i2c_wr_u16(ctx, 0x0102, 0x0001);
+    mdelay(30);
+	i2c_table_write(ctx, uTnpArrayInit_4lane,
+			ARRAY_SIZE(uTnpArrayInit_4lane));
+	DRV_LOG(ctx, "X\n");
+}
+
+static int open(struct subdrv_ctx *ctx)
+{
+	u32 sensor_id = 0;
+	u32 scenario_id = 0;
+	/* get sensor id */
+	if (common_get_imgsensor_id(ctx, &sensor_id) != ERROR_NONE)
+		return ERROR_SENSOR_CONNECT_FAIL;
+	/* initail setting */
+	s5kjd1_sensor_init(ctx);
+	memset(ctx->exposure, 0, sizeof(ctx->exposure));
+	memset(ctx->ana_gain, 0, sizeof(ctx->gain));
+	ctx->exposure[0] = ctx->s_ctx.exposure_def;
+	ctx->ana_gain[0] = ctx->s_ctx.ana_gain_def;
+	ctx->current_scenario_id = scenario_id;
+	ctx->pclk = ctx->s_ctx.mode[scenario_id].pclk;
+	ctx->line_length = ctx->s_ctx.mode[scenario_id].linelength;
+	ctx->frame_length = ctx->s_ctx.mode[scenario_id].framelength;
+	ctx->current_fps = 10 * ctx->pclk / ctx->line_length / ctx->frame_length;
+	ctx->readout_length = ctx->s_ctx.mode[scenario_id].readout_length;
+	ctx->read_margin = ctx->s_ctx.mode[scenario_id].read_margin;
+	ctx->min_frame_length = ctx->frame_length;
+	ctx->autoflicker_en = FALSE;
+	ctx->test_pattern = 0;
+	ctx->ihdr_mode = 0;
+	ctx->pdaf_mode = 0;
+	ctx->hdr_mode = 0;
+	ctx->extend_frame_length_en = 0;
+	ctx->is_seamless = 0;
+	ctx->fast_mode_on = 0;
+	ctx->sof_cnt = 0;
+	ctx->ref_sof_cnt = 0;
+	ctx->is_streaming = 0;
+	return ERROR_NONE;
+} /* open */
+
+static int s5kjd1_set_ctrl_locker(struct subdrv_ctx *ctx,
+		u32 cid, bool *is_lock)
+{
+	bool lock_set_ctrl = false;
+
+	if (unlikely(is_lock == NULL)) {
+		pr_info("[%s][ERROR] is_lock %p is NULL\n", __func__, is_lock);
+		return -EINVAL;
+	}
+
+	switch (cid) {
+	case V4L2_CID_MTK_STAGGER_AE_CTRL:
+	case V4L2_CID_MTK_MAX_FPS:
+		if ((ctx->sof_no == 0) && (ctx->is_streaming)) {
+			lock_set_ctrl = true;
+			DRV_LOG(ctx,
+				"[%s] Target lock cid(%u) lock_set_ctrl(%d), sof_no(%d) is_streaming(%d)\n",
+				__func__,
+				cid,
+				lock_set_ctrl,
+				ctx->sof_no,
+				ctx->is_streaming);
+		}
+		break;
+	default:
+		break;
+	}
+
+	*is_lock = lock_set_ctrl;
+	return ERROR_NONE;
+} /* s5kjd1_set_ctrl_locker */
