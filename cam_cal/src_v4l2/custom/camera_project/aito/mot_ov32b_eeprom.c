60b98c5b082b5ce30ebea112e3d8a30808f1960b
wangzl37 <wangzl37@motorola.com>
2024-03-19 17:39:50 +0800

    aito: Fix MNF Info read failure.
    Since CAMERA_CAM_CAL_DATA_MANUFACTURE command is appended after MTK's
    reference code in ENUM_CAMERA_CAM_CAL_TYPE_ENUM, the new command was
    added before CAMERA_CAM_CAL_DATA_MANUFACTURE, meanwhile the EEPROM data
    processing in driver is indexed by command, so driver processing has
    shift with command.
    This change is for correcting the shift.
    
    Change-Id: Ie8d9d5b0994fd1685df5b0f9c7f15df020740336
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2910363
    Reviewed-by: <mingjw1@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- cam_cal/src_v4l2/custom/camera_project/aito/mot_ov32b_eeprom.c
+++ cam_cal/src_v4l2/custom/camera_project/aito/mot_ov32b_eeprom.c
@@ -38,6 +38,7 @@ static struct STRUCT_CALIBRATION_LAYOUT_STRUCT cal_layout_table = {
 		{0x00000000, 0x00000000, 0x00000000, do_stereo_data},
 		{0x00000000, 0x00000000, 0x00000000, do_dump_all},
 		{0x00000000, 0x00000000, 0x00000000, do_lens_id},
+		{0x00000000, 0x00000000, 0x00000000, NULL},
 		{0x00000001, 0x00000000, 0x00000025, mot_do_manufacture_info}
 	}
 };
