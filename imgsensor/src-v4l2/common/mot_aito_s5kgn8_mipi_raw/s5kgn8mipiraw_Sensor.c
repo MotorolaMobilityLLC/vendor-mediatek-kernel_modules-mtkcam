88d8643cbcb2424c7c84b501b061078c3650e82d
wangzl37 <wangzl37@motorola.com>
2024-03-21 17:10:58 +0800

    aito: fix s5kgn8 long exposure capture cancel fail
    aito:
    --fix s5kgn8 long exposure capture cancel fail
    
    Change-Id: I0cf8e50b306121faac1205182df47c8701e8cead
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2913485
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- imgsensor/src-v4l2/common/mot_aito_s5kgn8_mipi_raw/s5kgn8mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/mot_aito_s5kgn8_mipi_raw/s5kgn8mipiraw_Sensor.c
@@ -1247,7 +1247,7 @@ static struct subdrv_static_ctx static_ctx = {
 	.checksum_value = 0x31E3FBE2,
 
 	/* custom stream control delay timing for hw limitation (ms) */
-	.custom_stream_ctrl_delay = 3,
+	.custom_stream_ctrl_delay = 0,
 };
 
 static struct subdrv_ops ops = {
