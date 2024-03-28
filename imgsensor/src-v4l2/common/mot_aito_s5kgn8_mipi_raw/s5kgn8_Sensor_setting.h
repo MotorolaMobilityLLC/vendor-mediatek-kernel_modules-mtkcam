c93659cf90ff38d089c10ff1164ee0d702f4c516
wangzl37 <wangzl37@motorola.com>
2024-03-28 18:30:28 +0800

    aito: s5kgn8 update setting(2.62) for fix dark shading tuning for 1st and 2nd module
    aito:
    --s5kgn8 update setting(2.62) for fix dark shading tuning for 1st and 2nd module
    
    Change-Id: I50516040bf7bbae401a295158b3238ad151ce15f
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2921591
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- imgsensor/src-v4l2/common/mot_aito_s5kgn8_mipi_raw/s5kgn8_Sensor_setting.h
+++ imgsensor/src-v4l2/common/mot_aito_s5kgn8_mipi_raw/s5kgn8_Sensor_setting.h
@@ -21,7 +21,7 @@
 #include "kd_camera_typedef.h"
 
 
-
+//common
 static u16 uTnpArrayInit_1[] = {
 #include "setting/s5kgn8_initsettting_1.h"
 };
@@ -36,17 +36,24 @@ static u16 uTnpArrayInit_3[] = {
 #include "setting/init_end.h"
 };
 
-static u16 addr_data_pair_preview[] = {
-#include "setting/s5kgn8_4096_3072_30fps.h"
+
+//OV
+static u16 uTnpArray_ov_gos[] = {
+#include "setting/OV/gos_ov.h"
 };
 
 
-static u16 s5kgn8_seamless_preview[] = {
+static u16 OV_addr_data_pair_preview[] = {
+#include "setting/OV/s5kgn8_4096_3072_30fps.h"
+};
+
+
+static u16 OV_s5kgn8_seamless_preview[] = {
 0xFCFC, 0x4000,
 #include "setting/scm_index.h"
 0xFCFC, 0x4000,
 0x0104, 0x0101,
-#include "setting/s5kgn8_4096_3072_30fps_sub.h"
+#include "setting/OV/s5kgn8_4096_3072_30fps_sub.h"
 0xFCFC, 0x4000,
 0x0B32, 0x0100,
 0x0B30, 0x0101,
@@ -54,20 +61,20 @@ static u16 s5kgn8_seamless_preview[] = {
 0x0104, 0x0001,
 };
 
-static u16 addr_data_pair_capture[] = {
-#include "setting/s5kgn8_4096_3072_30fps.h"
+static u16 OV_addr_data_pair_capture[] = {
+#include "setting/OV/s5kgn8_4096_3072_30fps.h"
 };
-static u16 addr_data_pair_normal_video[] = {
-#include "setting/s5kgn8_4096_2304_30fps.h"
+static u16 OV_addr_data_pair_normal_video[] = {
+#include "setting/OV/s5kgn8_4096_2304_30fps.h"
 };
 
 
-static u16 s5kgn8_seamless_normal_video[] = {
+static u16 OV_s5kgn8_seamless_normal_video[] = {
 0xFCFC, 0x4000,
 #include "setting/scm_index.h"
 0xFCFC, 0x4000,
 0x0104, 0x0101,
-#include "setting/s5kgn8_4096_2304_30fps_sub.h"
+#include "setting/OV/s5kgn8_4096_2304_30fps_sub.h"
 0xFCFC, 0x4000,
 0x0B32, 0x0100,
 0x0B30, 0x0100,
@@ -75,36 +82,36 @@ static u16 s5kgn8_seamless_normal_video[] = {
 0x0104, 0x0001,
 };
 
-static u16 addr_data_pair_hs_video[] = {
-#include "setting/s5kgn8_2048_1152_120fps.h"
+static u16 OV_addr_data_pair_hs_video[] = {
+#include "setting/OV/s5kgn8_2048_1152_120fps.h"
 };
 
-static u16 addr_data_pair_slim_video[] = {
-#include "setting/s5kgn8_4096_3072_30fps.h"
+static u16 OV_addr_data_pair_slim_video[] = {
+#include "setting/OV/s5kgn8_4096_3072_30fps.h"
 };
-static u16 addr_data_pair_custom1[] = {
-#include "setting/s5kgn8_8192_6144_24fps.h"
-};
-
-static u16 addr_data_pair_custom2[] = {
-#include "setting/s5kgn8_4096_2304_60fps.h"
+static u16 OV_addr_data_pair_custom1[] = {
+#include "setting/OV/s5kgn8_8192_6144_24fps.h"
 };
 
-static u16 addr_data_pair_custom3[] = {
-#include "setting/s5kgn8_2048_1152_240fps.h"
+static u16 OV_addr_data_pair_custom2[] = {
+#include "setting/OV/s5kgn8_4096_2304_60fps.h"
 };
 
-static u16 addr_data_pair_custom4[] = {
-#include "setting/s5kgn8_4096_2304_30fps_IDCG.h"
+static u16 OV_addr_data_pair_custom3[] = {
+#include "setting/OV/s5kgn8_2048_1152_240fps.h"
+};
+
+static u16 OV_addr_data_pair_custom4[] = {
+#include "setting/OV/s5kgn8_4096_2304_30fps_IDCG.h"
 };
 
 
-static u16 s5kgn8_seamless_custom4[] = {
+static u16 OV_s5kgn8_seamless_custom4[] = {
 0xFCFC, 0x4000,
 #include "setting/scm_index.h"
 0xFCFC, 0x4000,
 0x0104, 0x0101,
-#include "setting/s5kgn8_4096_2304_30fps_IDCG_sub.h"
+#include "setting/OV/s5kgn8_4096_2304_30fps_IDCG_sub.h"
 0xFCFC, 0x4000,
 0x0B32, 0x0100,
 0x0B30, 0x0101,
@@ -113,22 +120,134 @@ static u16 s5kgn8_seamless_custom4[] = {
 };
 
 //3rd video call
-static u16 addr_data_pair_custom5[] = {
-#include "setting/s5kgn8_2048_1536_30fps.h"
+static u16 OV_addr_data_pair_custom5[] = {
+#include "setting/OV/s5kgn8_2048_1536_30fps.h"
 };
 
 
 //in sensor zoom
-static u16 addr_data_pair_custom6[] = {
-#include "setting/s5kgn8_4096_3072_30fps_crop.h"
+static u16 OV_addr_data_pair_custom6[] = {
+#include "setting/OV/s5kgn8_4096_3072_30fps_crop.h"
 };
 
-static u16 s5kgn8_seamless_custom6[] = {
+static u16 OV_s5kgn8_seamless_custom6[] = {
 0xFCFC, 0x4000,
 #include "setting/scm_index.h"
 0xFCFC, 0x4000,
 0x0104, 0x0101,
-#include "setting/s5kgn8_4096_3072_30fps_crop_sub.h"
+#include "setting/OV/s5kgn8_4096_3072_30fps_crop_sub.h"
+0xFCFC, 0x4000,
+0x0B32, 0x0100,
+0x0B30, 0x0100,
+0x0340, 0x15CC,
+0x0104, 0x0001,
+};
+
+
+
+
+
+
+
+//QT
+
+static u16 uTnpArray_qt_gos[] = {
+#include "setting/QT/gos_qt.h"
+};
+
+static u16 QT_addr_data_pair_preview[] = {
+#include "setting/QT/s5kgn8_4096_3072_30fps.h"
+};
+
+
+static u16 QT_s5kgn8_seamless_preview[] = {
+0xFCFC, 0x4000,
+#include "setting/scm_index.h"
+0xFCFC, 0x4000,
+0x0104, 0x0101,
+#include "setting/QT/s5kgn8_4096_3072_30fps_sub.h"
+0xFCFC, 0x4000,
+0x0B32, 0x0100,
+0x0B30, 0x0101,
+0x0340, 0x199A,
+0x0104, 0x0001,
+};
+
+static u16 QT_addr_data_pair_capture[] = {
+#include "setting/QT/s5kgn8_4096_3072_30fps.h"
+};
+static u16 QT_addr_data_pair_normal_video[] = {
+#include "setting/QT/s5kgn8_4096_2304_30fps.h"
+};
+
+
+static u16 QT_s5kgn8_seamless_normal_video[] = {
+0xFCFC, 0x4000,
+#include "setting/scm_index.h"
+0xFCFC, 0x4000,
+0x0104, 0x0101,
+#include "setting/QT/s5kgn8_4096_2304_30fps_sub.h"
+0xFCFC, 0x4000,
+0x0B32, 0x0100,
+0x0B30, 0x0100,
+0x0340, 0x199A,
+0x0104, 0x0001,
+};
+
+static u16 QT_addr_data_pair_hs_video[] = {
+#include "setting/QT/s5kgn8_2048_1152_120fps.h"
+};
+
+static u16 QT_addr_data_pair_slim_video[] = {
+#include "setting/QT/s5kgn8_4096_3072_30fps.h"
+};
+static u16 QT_addr_data_pair_custom1[] = {
+#include "setting/QT/s5kgn8_8192_6144_24fps.h"
+};
+
+static u16 QT_addr_data_pair_custom2[] = {
+#include "setting/QT/s5kgn8_4096_2304_60fps.h"
+};
+
+static u16 QT_addr_data_pair_custom3[] = {
+#include "setting/QT/s5kgn8_2048_1152_240fps.h"
+};
+
+static u16 QT_addr_data_pair_custom4[] = {
+#include "setting/QT/s5kgn8_4096_2304_30fps_IDCG.h"
+};
+
+
+static u16 QT_s5kgn8_seamless_custom4[] = {
+0xFCFC, 0x4000,
+#include "setting/scm_index.h"
+0xFCFC, 0x4000,
+0x0104, 0x0101,
+#include "setting/QT/s5kgn8_4096_2304_30fps_IDCG_sub.h"
+0xFCFC, 0x4000,
+0x0B32, 0x0100,
+0x0B30, 0x0101,
+0x0340, 0x0AE4,
+0x0104, 0x0001,
+};
+
+//3rd video call
+static u16 QT_addr_data_pair_custom5[] = {
+#include "setting/QT/s5kgn8_2048_1536_30fps.h"
+};
+
+
+//in sensor zoom
+static u16 QT_addr_data_pair_custom6[] = {
+#include "setting/QT/s5kgn8_4096_3072_30fps_crop.h"
+};
+
+static u16 QT_s5kgn8_seamless_custom6[] = {
+0xFCFC, 0x4000,
+#include "setting/scm_index.h"
+0xFCFC, 0x4000,
+0x0104, 0x0101,
+#include "setting/QT/s5kgn8_4096_3072_30fps_crop_sub.h"
 0xFCFC, 0x4000,
 0x0B32, 0x0100,
 0x0B30, 0x0100,
