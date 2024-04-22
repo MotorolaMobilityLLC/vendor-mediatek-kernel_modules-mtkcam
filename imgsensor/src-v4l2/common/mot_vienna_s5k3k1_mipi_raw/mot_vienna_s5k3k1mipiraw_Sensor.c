bf59fc19b4394589f11bef2c4b57be90e84b754b
wangxiaofeng <wangxf32@motorola.com>
2024-04-22 17:26:50 +0800

    vienna: add s5k3k1 test pattern
    details:
    add s5k3k1 test pattern
    
    Change-Id: Id9c155975ab3c2012fc5bf9d6b6a2fe1004ec0ba
    Signed-off-by: wangxiaofeng <wangxf32@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2945318
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: wangdp11 <wangdp11@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- imgsensor/src-v4l2/common/mot_vienna_s5k3k1_mipi_raw/mot_vienna_s5k3k1mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/mot_vienna_s5k3k1_mipi_raw/mot_vienna_s5k3k1mipiraw_Sensor.c
@@ -640,9 +640,8 @@ static int mot_vienna_s5k3k1_set_test_pattern(struct subdrv_ctx *ctx, u8 *para, 
 
 	if (mode != ctx->test_pattern)
 		DRV_LOG(ctx, "mode(%u->%u)\n", ctx->test_pattern, mode);
-	/* 1:Solid Color 2:Color Bar 5:Black */
 	if (mode)
-		subdrv_i2c_wr_u16(ctx, 0x0600, mode); /*100% Color bar*/
+		subdrv_i2c_wr_u16(ctx, 0x0600, 0x0001); /*Black*/
 	else if (ctx->test_pattern)
 		subdrv_i2c_wr_u16(ctx, 0x0600, 0x0000); /*No pattern*/
 
