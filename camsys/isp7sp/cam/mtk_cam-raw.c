63e303f4324f4e5bdcf46af574810ad510347f06
guozy16 <guozy16@motorola.com>
2024-04-25 14:37:09 +0800

    Revert "(CR): aito: temporarily added some kernel log trace camera can't use issues"
    This reverts commit 4cf0cc5186750e842c99714557a076c0308bacf5.
    
    Change-Id: Ic8c3e69168f3c706f667cd891050c45682491264
    Reviewed-on: https://gerrit.mot.com/2949116
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Submit-Approved: Jira Key

--- camsys/isp7sp/cam/mtk_cam-raw.c
+++ camsys/isp7sp/cam/mtk_cam-raw.c
@@ -417,7 +417,7 @@ void apply_cq(struct mtk_raw_device *dev,
 {
 	dma_addr_t main, sub;
 
-//	if (CAM_DEBUG_ENABLED(RAW_INT))
+	if (CAM_DEBUG_ENABLED(RAW_INT))
 		dev_info(dev->dev,
 			 "apply raw%d cq - addr:0x%llx, size:%d/%d, offset:%d\n",
 			 dev->id, cq_addr, cq_size, sub_cq_size, sub_cq_offset);
@@ -1401,13 +1401,13 @@ static int mtk_raw_pm_suspend_prepare(struct mtk_raw_device *dev)
 //	u32 val;
 	int ret;
 
-	dev_info(dev->dev, "- %s\n", __func__);
+	dev_dbg(dev->dev, "- %s\n", __func__);
 
 	if (pm_runtime_suspended(dev->dev))
 		return 0;
 
 	/* Disable ISP's view finder and wait for TG idle */
-	dev_info(dev->dev, "cam suspend, disable VF\n");
+	dev_dbg(dev->dev, "cam suspend, disable VF\n");
 #ifdef NOT_READY
 	val = readl(dev->base + REG_TG_VF_CON);
 	writel(val & (~TG_VFDATA_EN), dev->base + REG_TG_VF_CON);
@@ -1415,7 +1415,7 @@ static int mtk_raw_pm_suspend_prepare(struct mtk_raw_device *dev)
 					(val & TG_CAM_CS_MASK) == TG_IDLE_ST,
 					USEC_PER_MSEC, MTK_RAW_STOP_HW_TIMEOUT);
 	if (ret)
-		dev_info(dev->dev, "can't stop HW:%d:0x%x\n", ret, val);
+		dev_dbg(dev->dev, "can't stop HW:%d:0x%x\n", ret, val);
 
 	/* Disable CMOS */
 	val = readl(dev->base + REG_TG_SEN_MODE);
@@ -1432,7 +1432,7 @@ static int mtk_raw_pm_post_suspend(struct mtk_raw_device *dev)
 //	u32 val;
 	int ret;
 
-	dev_info(dev->dev, "- %s\n", __func__);
+	dev_dbg(dev->dev, "- %s\n", __func__);
 
 	if (pm_runtime_suspended(dev->dev))
 		return 0;
@@ -1444,7 +1444,7 @@ static int mtk_raw_pm_post_suspend(struct mtk_raw_device *dev)
 
 #ifdef NOT_READY
 	/* Enable CMOS */
-	dev_info(dev->dev, "cam resume, enable CMOS/VF\n");
+	dev_dbg(dev->dev, "cam resume, enable CMOS/VF\n");
 	val = readl(dev->base + REG_TG_SEN_MODE);
 	writel(val | TG_SEN_MODE_CMOS_EN, dev->base + REG_TG_SEN_MODE);
 
@@ -1777,7 +1777,7 @@ static int mtk_raw_runtime_resume(struct device *dev)
 	if (pr_detect_count < KERNEL_LOG_MAX)
 		set_detect_count(KERNEL_LOG_MAX);
 
-//	if (CAM_DEBUG_ENABLED(RAW_INT))
+	if (CAM_DEBUG_ENABLED(RAW_INT))
 		dev_info(dev, "%s:enable clock\n", __func__);
 
 	mtk_mmdvfs_enable_vcp(true, VCP_PWR_USR_CAM);
@@ -2163,7 +2163,7 @@ static int mtk_yuv_runtime_suspend(struct device *dev)
 	struct mtk_yuv_device *drvdata = dev_get_drvdata(dev);
 	int i;
 
-//	if (CAM_DEBUG_ENABLED(RAW_INT))
+	if (CAM_DEBUG_ENABLED(RAW_INT))
 		dev_info(dev, "%s:disable clock\n", __func__);
 
 	mtk_cam_reset_qos(dev, &drvdata->qos);
@@ -2179,7 +2179,7 @@ static int mtk_yuv_runtime_resume(struct device *dev)
 	struct mtk_yuv_device *drvdata = dev_get_drvdata(dev);
 	int i, ret;
 
-//	if (CAM_DEBUG_ENABLED(RAW_INT))
+	if (CAM_DEBUG_ENABLED(RAW_INT))
 		dev_info(dev, "%s:enable clock\n", __func__);
 
 	enable_irq(drvdata->irq);
@@ -2590,7 +2590,7 @@ static int mtk_rms_runtime_suspend(struct device *dev)
 	struct mtk_rms_device *drvdata = dev_get_drvdata(dev);
 	int i;
 
-//	if (CAM_DEBUG_ENABLED(RAW_INT))
+	if (CAM_DEBUG_ENABLED(RAW_INT))
 		dev_info(dev, "%s:disable clock\n", __func__);
 
 	for (i = 0; i < drvdata->num_clks; i++)
@@ -2604,7 +2604,7 @@ static int mtk_rms_runtime_resume(struct device *dev)
 	struct mtk_rms_device *drvdata = dev_get_drvdata(dev);
 	int i, ret;
 
-//	if (CAM_DEBUG_ENABLED(RAW_INT))
+	if (CAM_DEBUG_ENABLED(RAW_INT))
 		dev_info(dev, "%s:enable clock\n", __func__);
 
 	for (i = 0; i < drvdata->num_clks; i++) {
