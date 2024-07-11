45bfb6d084c1afc73bf7f26f58c2277689d5af35
Yicheng Chen <yicheng.chen@mediatek.com>
2024-07-11 09:29:09 +0800

    [ALPS09055671] camsys: isp8: refactor qof availability check
    [Description]
    move qof_init_timer_freq() out of qof availability check
    since it belongs to hardware ddren control that exists
    both in mt6991 and mt6899.
    
    [Test]
    pdas
    
    MTK-Commit-Id: 17ecf96f4c1740282d35544fc66b11b883efb03f
    
    Signed-off-by: Yicheng Chen <yicheng.chen@mediatek.com>
    CR-Id: ALPS09055671
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ib77e49b33d49e68280b3b95f554704055a09733e

--- camsys/isp8/cam/mtk_cam-job.c
+++ camsys/isp8/cam/mtk_cam-job.c
@@ -691,10 +691,11 @@ mtk_cam_job_initialize_engines(struct mtk_cam_ctx *ctx,
 			raw = dev_get_drvdata(ctx->hw_raw[i]);
 			is_master = !!(raw_master_id == raw->id);
 
-			if (check_qof_support(job)) {
-				qof_init_timer_freq(raw);
+			/* the necessity of hw ddren */
+			qof_init_timer_freq(raw);
+
+			if (check_qof_support(job))
 				mtk_cam_enable_itc(raw);
-			}
 
 			initialize(raw, &engine_cb, !is_master, is_srt,
 				get_sensor_interval_us(job));
@@ -2636,10 +2637,11 @@ static int job_raw_change_hw_init(struct mtk_cam_job *job)
 					continue;
 				raw = dev_get_drvdata(ctx->hw_raw[i]);
 
-				if (check_qof_support(job)) {
-					qof_init_timer_freq(raw);
+				/* the necessity of hw ddren */
+				qof_init_timer_freq(raw);
+
+				if (check_qof_support(job))
 					mtk_cam_enable_itc(raw);
-				}
 
 				// TODO: replace "0x7"
 				if (BIT(raw->id) == (selected_need_init & 0x7))
