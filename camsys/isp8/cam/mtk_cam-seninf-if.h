92c08b08ff926036b3048ac27c4f3306d932504c
Auto Li <auto.li@mediatek.com>
2024-08-06 15:54:44 +0800

    [ALPS09169971] cam: assert seninf when dump error
    [Description]
    Assert seninf when dump error.
    
    [Test]
    Assertion test
    
    MTK-Commit-Id: c197c3145b4873a41151928780a4cf6c19dcfa0d
    
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS09169971
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I76e2d2be0fa73fbebfa68d6311b8c4f7a70f6e4c

--- camsys/isp8/cam/mtk_cam-seninf-if.h
+++ camsys/isp8/cam/mtk_cam-seninf-if.h
@@ -119,10 +119,11 @@ int mtk_cam_seninf_get_pixelrate(struct v4l2_subdev *sd, s64 *pixelrate);
 int mtk_cam_seninf_calc_pixelrate(struct device *dev, s64 width, s64 height, s64 hblank,
 				  s64 vblank, int fps_n, int fps_d, s64 sensor_pixel_rate);
 
-int mtk_cam_seninf_dump(struct v4l2_subdev *sd, u32 seq_id, bool force_check);
+int mtk_cam_seninf_dump(struct v4l2_subdev *sd, u32 seq_id, bool force_check,
+			bool assert_when_error);
 int mtk_cam_seninf_get_csi_irq_status(struct v4l2_subdev *sd, struct v4l2_ctrl *ctrl);
 
-int mtk_cam_seninf_dump_current_status(struct v4l2_subdev *sd);
+int mtk_cam_seninf_dump_current_status(struct v4l2_subdev *sd, bool assert_when_error);
 
 int mtk_cam_seninf_check_timeout(struct v4l2_subdev *sd, u64 time_waited);
 u64 mtk_cam_seninf_get_frame_time(struct v4l2_subdev *sd, u32 seq_id);
