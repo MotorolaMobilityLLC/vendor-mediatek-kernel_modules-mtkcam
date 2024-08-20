8f50a2cc21b0ad15cb97a3b266ffb045e9faf905
Yuming Wei <yu-ming.wei@mediatek.com>
2024-08-20 09:01:29 +0800

    [ALPS09227590] camsys: isp8: add seninf debug dump abort
    [Description]
    add seninf debug dump abort when stream off
    [Test]
    pdas
    
    MTK-Commit-Id: 9c7aaf03d7a8958e59d278013dab15221346ff16
    
    Signed-off-by: Yuming Wei <yu-ming.wei@mediatek.com>
    CR-Id: ALPS09227590
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I979cdca4d2050502b5e6f3424b5c2668bb06764b

--- camsys/isp8/cam/mtk_cam-ctrl.c
+++ camsys/isp8/cam/mtk_cam-ctrl.c
@@ -2229,7 +2229,8 @@ void mtk_cam_ctrl_stop(struct mtk_cam_ctrl *cam_ctrl)
 		call_jobop(job, cancel);
 	}
 	read_unlock(&cam_ctrl->list_lock);
-
+	if (ctx->seninf)
+		mtk_cam_seninf_set_abort(ctx->seninf);
 	mtk_cam_watchdog_stop(&cam_ctrl->watchdog);
 
 	/* this would be time consuming */
