45d566a35263f9293d2fc44b685e26ae854d4977
Yuming Wei <yu-ming.wei@mediatek.com>
2024-08-20 09:01:29 +0800

    [ALPS08058990] camsys: isp8: fix slc req/release pairs
    [Description]
    fix slc req/release pairs
    
    [Test]
    pdas
    
    MTK-Commit-Id: a7ce99d9ce251aa6a54b5a7e03ef3f399a65cf5e
    
    Signed-off-by: Yuming Wei <yu-ming.wei@mediatek.com>
    CR-Id: ALPS08058990
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I6c8fa9d7e9d0265cdbe007e22812ce41d8076655

--- camsys/isp8/cam/mtk_cam.c
+++ camsys/isp8/cam/mtk_cam.c
@@ -2116,10 +2116,12 @@ static int mtk_cam_ctx_release_slc(struct mtk_cam_ctx *ctx)
 {
 	int ret = 0;
 #if IS_ENABLED(CONFIG_MTK_SLBC)
-	ret = slbc_gid_release(ID_CAM, ctx->slc_gid);
-	dev_info(ctx->cam->dev, "%s: slc_data bw/dma size:%d/%d\n", __func__,
-		ctx->slc_data.bw, ctx->slc_data.dma_size);
-	ctx->slc_data_valid = false;
+	if (ctx->slc_data_valid) {
+		ret = slbc_gid_release(ID_CAM, ctx->slc_gid);
+		dev_info(ctx->cam->dev, "%s: slc_data bw/dma size:%d/%d\n", __func__,
+			ctx->slc_data.bw, ctx->slc_data.dma_size);
+		ctx->slc_data_valid = false;
+	}
 #endif
 	return ret;
 }
