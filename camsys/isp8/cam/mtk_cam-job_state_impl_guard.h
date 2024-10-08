1249a92315c4c790167224500c851b9d898dd5c1
Yuming Wei <yu-ming.wei@mediatek.com>
2024-10-08 09:30:18 +0800

    [ALPS09394045] camsys: isp8: smvr apply cq from ack check sof ts
    [Description]
    smvr apply cq from ack check sof ts
    
    [Test]
    pdas
    
    MTK-Commit-Id: 2902be87e8f19bf22f8b927ff2a55375c381330a
    
    Signed-off-by: Yuming Wei <yu-ming.wei@mediatek.com>
    CR-Id: ALPS09394045
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Id9995622afeb89fe2df4aa4809c0ca6018872852

--- camsys/isp8/cam/mtk_cam-job_state_impl_guard.h
+++ camsys/isp8/cam/mtk_cam-job_state_impl_guard.h
@@ -308,7 +308,8 @@ static inline bool valid_cq_execution_subsample(
 		return ret;
 
 	spin_lock(p->info_lock);
-	ret = ((p->event_ts - p->info->sof_ts_ns) < p->cq_trigger_thres)&&
+	ret = (p->info->sof_ts_ns <= p->info->sof_l_ts_ns) &&
+		((p->event_ts - p->info->sof_ts_ns) < p->cq_trigger_thres) &&
 		(s_acc->seq_no == p->info->inner_seq_no + 1);
 	if (ret == false)
 		pr_info("[mtk-cam:vld_cq_exesubsample:%d/%d] event/l_sof/cq:%llu/%llu/%llu sof:%llu(%llu)",
