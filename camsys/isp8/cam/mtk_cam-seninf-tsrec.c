5946064a4ff64148935eb81eb46d65432c2194df
mtk31366 <siao-cing.huang@mediatek.com>
2024-10-29 10:02:08 +0800

    [ALPS09426377] cam: TSREC clr iqr status timing
    [Description]
    cam: TSREC clr iqr status timing
    
    [Test]
    local PDAS, stress test
    
    MTK-Commit-Id: 165e2124cff374b76975e7313f636c4f1c0df54a
    
    Signed-off-by: mtk31366 <siao-cing.huang@mediatek.com>
    CR-Id: ALPS09426377
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: If01a77a118ce5f424a3c6908e33169feabad4330

--- camsys/isp8/cam/mtk_cam-seninf-tsrec.c
+++ camsys/isp8/cam/mtk_cam-seninf-tsrec.c
@@ -2275,6 +2275,30 @@ static inline void tsrec_intr_en_bits_update(const unsigned int idx,
 	}
 }
 
+static inline void tsrec_force_clr_intr_status(const unsigned int tsrec_no)
+{
+	unsigned int intr_status;
+
+	/* after disable intr en bit, force clr intr status register for */
+	/* preventing irq handler find out intr en is clr and then skip handle */
+	intr_status = mtk_cam_seninf_g_tsrec_n_intr_status(tsrec_no);
+	if (intr_status) {
+		/* please call this API whether write clear is enabled or not */
+		mtk_cam_seninf_clr_tsrec_n_intr_status(tsrec_no, intr_status);
+		TSREC_LOG_INF(
+			"NOTICE: tsrec_no:%u, intr_en_bits:%#x, intr_status:%#x => skip IRQ handle\n",
+			tsrec_no,
+			TSREC_ATOMIC_READ(&tsrec_status.intr_en_bits),
+			intr_status);
+		return;
+	}
+
+	TSREC_LOG_DBG(
+		"tsrec_no:%u, intr_en_bits:%#x, intr_status:%#x\n",
+		tsrec_no,
+		TSREC_ATOMIC_READ(&tsrec_status.intr_en_bits),
+		intr_status);
+}
 
 static void tsrec_intr_reset(const unsigned int tsrec_no)
 {
@@ -2305,6 +2329,9 @@ static void tsrec_intr_reset(const unsigned int tsrec_no)
 	mtk_cam_seninf_s_tsrec_n_intr_en(tsrec_no,
 		1, 1, 1, 1, 0);
 
+	/* after INTR disable, force clr INTR status */
+	tsrec_force_clr_intr_status(tsrec_no);
+
 	/* last, clear intr_en_bits after interrupt be disabled */
 	tsrec_intr_en_bits_update(tsrec_no, 0);
 
@@ -2316,32 +2343,6 @@ static void tsrec_intr_reset(const unsigned int tsrec_no)
 		TSREC_ATOMIC_READ(&p_tsrec_n_regs->intr_en),
 		0,
 		TSREC_ATOMIC_READ(&tsrec_status.intr_en_bits));
-}
-
-
-static void tsrec_force_clr_intr_status(const unsigned int tsrec_no)
-{
-	unsigned int intr_status;
-
-	/* after disable intr en bit, force clr intr status register for */
-	/* preventing irq handler find out intr en is clr and then skip handle */
-	intr_status = mtk_cam_seninf_g_tsrec_n_intr_status(tsrec_no);
-	if (intr_status) {
-		/* please call this API whether write clear is enabled or not */
-		mtk_cam_seninf_clr_tsrec_n_intr_status(tsrec_no, intr_status);
-		TSREC_LOG_INF(
-			"NOTICE: tsrec_no:%u, intr_en_bits:%#x, intr_status:%#x => skip IRQ handle\n",
-			tsrec_no,
-			TSREC_ATOMIC_READ(&tsrec_status.intr_en_bits),
-			intr_status);
-		return;
-	}
-
-	TSREC_LOG_DBG(
-		"tsrec_no:%u, intr_en_bits:%#x, intr_status:%#x\n",
-		tsrec_no,
-		TSREC_ATOMIC_READ(&tsrec_status.intr_en_bits),
-		intr_status);
 }
 
 
@@ -2693,8 +2694,6 @@ static void tsrec_n_settings_clear(const unsigned int tsrec_no)
 	/* INTR disable */
 	tsrec_intr_reset(tsrec_no);
 
-	/* after INTR disable, force clr INTR status */
-	tsrec_force_clr_intr_status(tsrec_no);
 
 	/* reset exp vc dt info */
 	tsrec_seninf_exp_vc_dt_reset(tsrec_no);
