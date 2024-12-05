b0e278bc8cf1a4fdf38db7b42cd797b0fc2a3722
yangmf3 <yangmf3@motorola.com>
2024-12-05 16:20:36 +0800

    scout: update gc32e1 avdd power-up delay
    details:
    update avdd power-up delay to 3ms
    
    Change-Id: Iaab1d98ef01bcec5534b40ef9555c85ebaceca6d
    Signed-off-by: yangmf3 <yangmf3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3163386
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- imgsensor/src-v4l2/common/mot_scout_gc32e1_mipi_raw/mot_scout_gc32e1mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/mot_scout_gc32e1_mipi_raw/mot_scout_gc32e1mipiraw_Sensor.c
@@ -406,7 +406,7 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK_DRIVING_CURRENT, 4, 0},
 	{HW_ID_DOVDD, 1800000, 1},
 	{HW_ID_DVDD, 1200000, 1},
-	{HW_ID_AVDD, 2800000, 1},
+	{HW_ID_AVDD, 2800000, 3},
 	{HW_ID_RST, 1, 5},
 };
 
