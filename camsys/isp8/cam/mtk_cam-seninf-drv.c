c086d56b054188048b219d0af98cdad72de8719a
mtk28436 <larry.chung@mediatek.com>
2024-12-12 20:55:38 +0800

    [ALPS09534840] cam: remove subdev register out of lock
    [Description]
    v4l2_dev in k-mainline add videodev_lock in video_device_register,
    it will cause seninf hold graph_mutex -> videodev_lock -> graph_mutex,
    which also cause the tool detects deadlock risk in this repeated order.
    Therefore, we remove this out of graph_mutex holding.
    [Test]
    tc29/30
    
    MTK-Commit-Id: bca469947e9a065b5c3c7d94d2a931f68f01a0d3
    
    CR-Id: ALPS09534840
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: mtk28436 <larry.chung@mediatek.com>
    Change-Id: I2b7139e8b41a1d4da02c0641058611b253be0d92

--- camsys/isp8/cam/mtk_cam-seninf-drv.c
+++ camsys/isp8/cam/mtk_cam-seninf-drv.c
@@ -2830,21 +2830,19 @@ static int seninf_notifier_bound(struct v4l2_async_notifier *notifier,
 	ret = media_create_pad_link(&sd->entity, 0,
 				    &ctx->subdev.entity, 0,
 				    MEDIA_LNK_FL_DYNAMIC);
+	mutex_unlock(&ctx->subdev.v4l2_dev->mdev->graph_mutex);
 	if (ret) {
 		dev_info(ctx->dev,
 			"failed to create link for %s\n",
 			sd->entity.name);
-		mutex_unlock(&ctx->subdev.v4l2_dev->mdev->graph_mutex);
 		return ret;
 	}
 
 	ret = v4l2_device_register_subdev_nodes(ctx->subdev.v4l2_dev);
 	if (ret) {
 		dev_info(ctx->dev, "failed to create subdev nodes\n");
-		mutex_unlock(&ctx->subdev.v4l2_dev->mdev->graph_mutex);
 		return ret;
 	}
-	mutex_unlock(&ctx->subdev.v4l2_dev->mdev->graph_mutex);
 	dev_info(ctx->dev, "%s bounded exit\n", sd->entity.name);
 
 	return 0;
