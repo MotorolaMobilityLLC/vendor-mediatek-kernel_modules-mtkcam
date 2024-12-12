b62105c63f345577ebfd6c84acd2e856c8e63486
Yuming Wei <yu-ming.wei@mediatek.com>
2024-12-12 20:51:53 +0800

    [ALPS08058990] camsys: isp8: seamless check timestamp by fps
    [Description]
    1 . seamless check timestamp by fps
    2 . add smvr state machine apply cq log
    
    [Test]
    pdas
    
    MTK-Commit-Id: 7379ade2f4f4f1d8e79972f076642c1fb24ea657
    
    Signed-off-by: Yuming Wei <yu-ming.wei@mediatek.com>
    CR-Id: ALPS08058990
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ic88901e798ee70bae37ffa97ffbed37f3d123b0d
    (cherry picked from commit fcfe40e1c6e9ab44eef0e4dc8c6d918d2de2ffd9)

--- camsys/isp8/cam/mtk_cam-job_state_impl_guard.h
+++ camsys/isp8/cam/mtk_cam-job_state_impl_guard.h
@@ -311,6 +311,11 @@ static inline bool valid_cq_execution_subsample(
 	ret = (p->info->sof_ts_ns <= p->info->sof_l_ts_ns) &&
 		((p->event_ts - p->info->sof_ts_ns) < p->cq_trigger_thres) &&
 		(s_acc->seq_no == p->info->inner_seq_no + 1);
+	if (ret == false)
+		pr_info("[mtk-cam:vld_cq_exesubsample:%d/%d] event/l_sof/cq:%llu/%llu/%llu sof:%llu(%llu)",
+			s_acc->seq_no, p->info->inner_seq_no,
+			p->event_ts, p->info->sof_l_ts_ns, p->cq_trigger_thres, p->info->sof_ts_ns,
+			ktime_get_boottime_ns());
 	spin_unlock(p->info_lock);
 
 	return ret;
