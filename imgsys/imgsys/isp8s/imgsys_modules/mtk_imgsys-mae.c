0fb5f07a2bfd25ca3df2f37ebbb9cedd705c4393
mtk22372 <ming-hsuan.chiang@mediatek.com>
2024-12-12 20:56:48 +0800

    [ALPS09195395] imgsys: mae: porting for Jayer
    [Description]
    1. adjust mae base address
    2. add dump rdma status
    
    [Test]
    UT pass
    
    MTK-Commit-Id: 93abf5276e226efad761d0c30dd0f23a3f08ee71
    
    Signed-off-by: mtk22372 <ming-hsuan.chiang@mediatek.com>
    CR-Id: ALPS09195395
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ia31080bd5d5494b5563bfc1ce066c849054e1bc5

--- imgsys/imgsys/isp8s/imgsys_modules/mtk_imgsys-mae.c
+++ imgsys/imgsys/isp8s/imgsys_modules/mtk_imgsys-mae.c
@@ -19,7 +19,7 @@
  *******************************************************************************/
 static void __iomem *g_maeRegBA;
 
-#define MAE_BASE  0x34310000
+#define MAE_BASE  0x34320000
 // 0x1000 MAE_CTRL_CENTER
 #define MAE_CTRL_CENTER_BASE              (0x1000)
 #define MAE_CTRL_CENTER_LEN               (0x308)
@@ -64,25 +64,55 @@ static void __iomem *g_maeRegBA;
 #define MAE_MAISR_APB_BASE                (0x6A00)
 #define MAE_MAISR_APB_LEN                 (0x1FC)
 
-// 0x7000 MMFD_POST
-#define MMFD_POST_BASE                    (0x7000)
-#define MMFD_POST_LEN                     (0x0114)
+// 0X6C00 MAE_POST0
+#define MAE_POST0_APB_BASE                (0x6C00)
+#define MAE_POST0_APB_LEN                 (0x11C)
+
+// 0X6E00 MAE_POST1
+#define MAE_POST1_APB_BASE                (0x6E00)
+#define MAE_POST1_APB_LEN                 (0x1FC)
+
+// 0x7000 MAE_MMFD_POST
+#define MAE_MMFD_POST_BASE                (0x7000)
+#define MAE_MMFD_POST_LEN                 (0x0160)
+
+// 0x7200 MAE_DRV_2
+#define MAE_DRV_2_BASE                    (0x7200)
+#define MAE_DRV_2_LEN                     (0x01D0)
 
 // 0x7400 MAE_DRV
 #define MAE_DRV_W_BASE                    (0x7400)
-#define MAE_DRV_W_LEN                     (0x0164)
+#define MAE_DRV_W_LEN                     (0x01CC)
 
 // 0x7600 MAE_DRV_R
 #define MAE_DRV_R_BASE                    (0x7600)
 #define MAE_DRV_R_LEN                     (0x01FC)
 
+// 0x7800 MAE_DRV_R_2
+#define MAE_DRV_R_2_BASE                  (0x7800)
+#define MAE_DRV_R_2_LEN                   (0x01FC)
+
+#define MAE_maisr_apb_reg_0170_maisr_apb  (0x6b70)
+#define MAE_DRV_R_reg_0178_MAE_DRV_R      (0x7778)
+#define MAE_DRV_R_reg_017C_MAE_DRV_R      (0x777C)
+
+// module param
+int g_imgsys_debug_opt;
+module_param(g_imgsys_debug_opt, int, 0644);
+
 /*******************************************************************************
- * static Functions
- *******************************************************************************/
+* static Functions
+*******************************************************************************/
+bool get_imgsys_debug_opt(void)
+{
+	return g_imgsys_debug_opt;
+}
+
 void mae_reg_dump(void)
 {
 	void __iomem *maeRegBA = 0L;
 	int i = 0;
+	unsigned int val;
 
 	/* iomap registers */
 	maeRegBA = g_maeRegBA;
@@ -90,6 +120,29 @@ void mae_reg_dump(void)
 		pr_info("%s Unable to ioremap MAE registers\n",
 			__func__);
 	}
+
+	// reset driver domain for read back compiler domain reg
+	val = (unsigned int)ioread32((void *)(maeRegBA + MAE_maisr_apb_reg_0170_maisr_apb));
+	iowrite32(val | (0x1 << 3), (void *)(maeRegBA + MAE_maisr_apb_reg_0170_maisr_apb));
+	iowrite32(val & ~(0x1 << 3), (void *)(maeRegBA + MAE_maisr_apb_reg_0170_maisr_apb));
+
+	val = (unsigned int)ioread32((void *)(maeRegBA + MAE_DRV_R_reg_0178_MAE_DRV_R));
+	iowrite32(val & (0xffffffe0),
+		(void *)(maeRegBA + MAE_DRV_R_reg_0178_MAE_DRV_R));
+	pr_info("%s: [0x%08X] %08X, sel = 0, [0x%08X] %08X", __func__,
+		(unsigned int)(MAE_BASE + MAE_DRV_R_reg_0178_MAE_DRV_R),
+		(unsigned int)ioread32((void *)(maeRegBA + MAE_DRV_R_reg_0178_MAE_DRV_R)),
+		(unsigned int)(MAE_BASE + MAE_DRV_R_reg_017C_MAE_DRV_R),
+		(unsigned int)ioread32((void *)(maeRegBA + MAE_DRV_R_reg_017C_MAE_DRV_R)));
+
+	val = (unsigned int)ioread32((void *)(maeRegBA + MAE_DRV_R_reg_0178_MAE_DRV_R));
+	iowrite32((val & (0xffffffe0)) + 1,
+		(void *)(maeRegBA + MAE_DRV_R_reg_0178_MAE_DRV_R));
+	pr_info("%s: [0x%08X] %08X, sel = 1, [0x%08X] %08X", __func__,
+		(unsigned int)(MAE_BASE + MAE_DRV_R_reg_0178_MAE_DRV_R),
+		(unsigned int)ioread32((void *)(maeRegBA + MAE_DRV_R_reg_0178_MAE_DRV_R)),
+		(unsigned int)(MAE_BASE + MAE_DRV_R_reg_017C_MAE_DRV_R),
+		(unsigned int)ioread32((void *)(maeRegBA + MAE_DRV_R_reg_017C_MAE_DRV_R)));
 
 	// 0x1000 MAE_CTRL_CENTER
 	for (i = MAE_CTRL_CENTER_BASE; i <= MAE_CTRL_CENTER_BASE + MAE_CTRL_CENTER_LEN; i += 0x10) {
@@ -201,8 +254,38 @@ void mae_reg_dump(void)
 			(unsigned int)ioread32((void *)(maeRegBA + (i+0xC))));
 	}
 
-	// 0x7000 MMFD_POST
-	for (i = MMFD_POST_BASE; i <= MMFD_POST_BASE + MMFD_POST_LEN; i += 0x10) {
+	// 0X6C00 MAE_POST0_APB_BASE
+	for (i = MAE_POST0_APB_BASE; i <= MAE_POST0_APB_BASE + MAE_POST0_APB_LEN; i += 0x10) {
+		pr_info("%s: [0x%08X] %08X, %08X, %08X, %08X", __func__,
+			(unsigned int)(MAE_BASE + i),
+			(unsigned int)ioread32((void *)(maeRegBA + i)),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x4))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x8))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0xC))));
+	}
+
+	// 0X6E00 MAE_POST1_APB_BASE
+	for (i = MAE_POST1_APB_BASE; i <= MAE_POST1_APB_BASE + MAE_POST1_APB_LEN; i += 0x10) {
+		pr_info("%s: [0x%08X] %08X, %08X, %08X, %08X", __func__,
+			(unsigned int)(MAE_BASE + i),
+			(unsigned int)ioread32((void *)(maeRegBA + i)),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x4))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x8))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0xC))));
+	}
+
+	// 0x7000 MAE_MMFD_POST
+	for (i = MAE_MMFD_POST_BASE; i <= MAE_MMFD_POST_BASE + MAE_MMFD_POST_LEN; i += 0x10) {
+		pr_info("%s: [0x%08X] %08X, %08X, %08X, %08X", __func__,
+			(unsigned int)(MAE_BASE + i),
+			(unsigned int)ioread32((void *)(maeRegBA + i)),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x4))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x8))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0xC))));
+	}
+
+	// 0x7200 MAE_DRV_2
+	for (i = MAE_DRV_2_BASE; i <= MAE_DRV_2_BASE + MAE_DRV_2_LEN; i += 0x10) {
 		pr_info("%s: [0x%08X] %08X, %08X, %08X, %08X", __func__,
 			(unsigned int)(MAE_BASE + i),
 			(unsigned int)ioread32((void *)(maeRegBA + i)),
@@ -223,6 +306,16 @@ void mae_reg_dump(void)
 
 	// 0x7600 MAE_DRV_R
 	for (i = MAE_DRV_R_BASE; i <= MAE_DRV_R_BASE + MAE_DRV_R_LEN; i += 0x10) {
+		pr_info("%s: [0x%08X] %08X, %08X, %08X, %08X", __func__,
+			(unsigned int)(MAE_BASE + i),
+			(unsigned int)ioread32((void *)(maeRegBA + i)),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x4))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0x8))),
+			(unsigned int)ioread32((void *)(maeRegBA + (i+0xC))));
+	}
+
+	// 0x7800 MAE_DRV_R_2
+	for (i = MAE_DRV_R_2_BASE; i <= MAE_DRV_R_2_BASE + MAE_DRV_R_2_LEN; i += 0x10) {
 		pr_info("%s: [0x%08X] %08X, %08X, %08X, %08X", __func__,
 			(unsigned int)(MAE_BASE + i),
 			(unsigned int)ioread32((void *)(maeRegBA + i)),
@@ -252,7 +345,23 @@ void imgsys_mae_init(struct mtk_imgsys_dev *imgsys_dev)
 
 void imgsys_mae_set(struct mtk_imgsys_dev *imgsys_dev)
 {
+#ifdef MAE_SET_INIT_VALUE
+	void __iomem *maeRegBA = 0L;
+
+	/* iomap registers */
+	maeRegBA = g_maeRegBA;
+	if (!maeRegBA) {
+		pr_info("%s Unable to ioremap MAE registers\n",
+			__func__);
+	}
+#endif
+
+	pr_info("%s: +\n", __func__);
+
+#ifdef MAE_SET_INIT_VALUE
 	// set default setting if need
+	iowrite32(0x00002000, (void *)(maeRegBA + 0x00001010));
+#endif
 }
 
 void imgsys_mae_debug_dump(struct mtk_imgsys_dev *imgsys_dev,
@@ -272,7 +381,7 @@ bool imgsys_mae_done_chk(struct mtk_imgsys_dev *imgsys_dev, uint32_t engine)
 	if (!(value & 0x1)) {
 		ret = false;
 		pr_info(
-		"%s: hw_comb:0x%x, polling ME done fail!!! [0x%08x] 0x%x",
+		"%s: hw_comb:0x%x, polling MAE done fail!!! [0x%08x] 0x%x",
 		__func__, engine,
 		(uint32_t)(MAE_BASE + 0x1018), value);
 	}
