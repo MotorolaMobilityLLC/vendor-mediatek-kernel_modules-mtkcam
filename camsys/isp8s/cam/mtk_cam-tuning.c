5e816688ae7c09c4788221e52e7f0073c2089594
Will Fong <will.fong@mediatek.com>
2024-12-12 20:56:38 +0800

    [ALPS09428233] camsys: clone isp8s
    [Description]
    1. build pass
    2. cherry pick:
        from: 88876dfe8c1d [ALPS09052877] camsys: validate cq apply
        til: d9b753cd5dd1 [ALPS09413672] camsys: reset csr
    
    3. changes
    
    [Test]
    Build pass and self-sanity pass
    
    MTK-Commit-Id: d9a480f73704be395b6d19aef9d728dc1e2e10af
    
    Signed-off-by: Will Fong <will.fong@mediatek.com>
    CR-Id: ALPS09428233
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I20966970d63b409ba245b7ae2287d9ecb2772598

---
+++ camsys/isp8s/cam/mtk_cam-tuning.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0
+//
+// Copyright (c) 2019 MediaTek Inc.
+
+#include "mtk_cam.h"
+
+void mtk_cam_tuning_probe(void)
+{
+}
+
+void mtk_cam_tuning_init(struct mtk_cam_tuning *param)
+{
+}
+
+void mtk_cam_tuning_update(struct mtk_cam_tuning *param)
+{
+}
