85e48235b2dbf97ac00a57f89d5a65d8ca489430
Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
2025-01-18 23:50:51 +0800

    [ALPS09424410] TRAW: Set end token = 0xC0000000
    [Description]
    Set end token = 0xC0000000
    
    [Test]
    Preview Pass
    
    MTK-Commit-Id: 52618fc15d264513a3926b130c4f9008414fd24e
    
    CR-Id: ALPS09424410
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
    Change-Id: I52b525c2756b4b9f7a150e50f1a91fdb8346f88b

--- imgsys/imgsys/isp8s/imgsys_modules/mtk_imgsys-traw.h
+++ imgsys/imgsys/isp8s/imgsys_modules/mtk_imgsys-traw.h
@@ -85,7 +85,7 @@
 #define	TRAW_DMA_DRZS4NO_T1_ADDR	(0x5540)
 #define	TRAW_DMA_LTMSO_T1_ADDR		(0x5600)
 
-#define TRAW_CQ_DESC_NUM 173 // align with userspace
+#define TRAW_CQ_DESC_NUM 180 // align with userspace
 #define TRAW_REG_SIZE  (0xEE00) // align with userspace
 #define TRAW_TDR_BUF_MAXSZ 122880 // align with userspace
 /********************************************************************
