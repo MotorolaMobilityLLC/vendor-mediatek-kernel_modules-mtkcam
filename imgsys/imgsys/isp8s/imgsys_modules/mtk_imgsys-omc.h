31fa9158836f411a2db7a78899ffceffaca2b400
mtk32734 <ken.yu@mediatek.com>
2025-01-18 23:48:00 +0800

    [ALPS09521862] omc: jayer ep kernel drv v2
    [Description]
    1. Modify Cq desc size
    
    [Test]
    None
    
    MTK-Commit-Id: df1bc154130fb2136ceb947a0bf94c1e97465bd6
    
    Signed-off-by: mtk32734 <ken.yu@mediatek.com>
    CR-Id: ALPS09521862
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ic099057d93572880c82bec4ff8af1ca0100d557c

--- imgsys/imgsys/isp8s/imgsys_modules/mtk_imgsys-omc.h
+++ imgsys/imgsys/isp8s/imgsys_modules/mtk_imgsys-omc.h
@@ -19,7 +19,7 @@
  ********************************************************************/
 
 #define OMC_UFOD_P2_DESC_OFST 20 // align with userspace
-#define OMC_CQ_DESC_NUM	31 // align with userspace, TODO: need to check this
+#define OMC_CQ_DESC_NUM	34 // align with userspace
 #define OMC_REG_SIZE 4096  // align with userspace
 #define OMC_TDR_BUF_MAXSZ 9216 // align with userspace, TODO: need to check this
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
