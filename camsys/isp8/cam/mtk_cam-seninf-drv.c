5ee2f88006c0ceee3dfc9a4fbf39fb116519104a
mtk28436 <larry.chung@mediatek.com>
2025-02-05 08:28:29 +0800

    [ALPS09726141] cam: fix wait outmux cfg done issue
    [Description]
    1. fix error wait time
    2. reset outmux when s_stream_mux
    
    [Test]
    MM PDAS/stability
    
    MTK-Commit-Id: 9383077a3fefcb7b64dc69248fe113125e5d71f0
    
    CR-Id: ALPS09726141
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: mtk28436 <larry.chung@mediatek.com>
    Change-Id: I3c428afe18428872689d3dd46905b320214c259a
    (cherry picked from commit 22dc4ad5b7df97c56b0b5fd16c2ee1724f5b0fa7)

--- camsys/isp8/cam/mtk_cam-seninf-drv.c
+++ camsys/isp8/cam/mtk_cam-seninf-drv.c
@@ -1653,6 +1653,7 @@ static void update_cfg_done_max_wait_time(struct seninf_ctx *ctx)
 	if (!ctx->is_test_model) {
 		frame_time = mtk_cam_seninf_get_frame_time(&ctx->subdev, 0 /* seq, unused */);
 		frame_time = frame_time + (frame_time / 10);  /* 110 percent frame time */
+		frame_time = frame_time/1000; /* convert to micro second */
 	}
 
 	seninf_logd(ctx, "The frame time is %llu us\n", frame_time);
@@ -3322,7 +3323,6 @@ static int seninf_close(struct v4l2_subdev *sd, struct v4l2_subdev_fh *fh)
 
 	mutex_lock(&ctx->mutex);
 	ctx->open_refcnt--;
-	ctx->is_aov_real_sensor = 0;
 
 	if (!ctx->open_refcnt) {
 		dev_info(ctx->dev, "%s open_refcnt %d\n", __func__, ctx->open_refcnt);
@@ -3356,6 +3356,8 @@ static int seninf_close(struct v4l2_subdev *sd, struct v4l2_subdev_fh *fh)
 			ctx->pid = NULL;
 		}
 	}
+
+	ctx->is_aov_real_sensor = 0;
 
 	mutex_unlock(&ctx->mutex);
 
