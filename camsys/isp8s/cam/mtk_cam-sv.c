e0021bb0a10794cc5333e881fcedee118d70fbe3
Wen-jie Pan <Wen-jie.Pan@mediatek.com>
2025-02-05 08:28:34 +0800

    [ALPS09440596] camsv: fixed v4l2 ctrl api
    [Description]
    1. fixed v4l2 ctrl api
    
    [Test]
    Build Pass
    
    MTK-Commit-Id: 2c04a25b2aad0b8b16790acb66b059505b2e2a94
    
    CR-Id: ALPS09440596
    Signed-off-by: Wen-jie Pan <Wen-jie.Pan@mediatek.com>
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I756edbfdbedf909a6df41485296a5bf902d9d56f

--- camsys/isp8s/cam/mtk_cam-sv.c
+++ camsys/isp8s/cam/mtk_cam-sv.c
@@ -1603,6 +1603,7 @@ void mtk_cam_update_sensor_resource(struct mtk_cam_ctx *ctx)
 		fi.pad = 0;
 		fi.reserved[0] = V4L2_SUBDEV_FORMAT_ACTIVE;
 #if (KERNEL_VERSION(6, 7, 0) < LINUX_VERSION_CODE)
+		fi.which = V4L2_SUBDEV_FORMAT_ACTIVE;
 		v4l2_subdev_call_state_active(ctx->sensor, pad, get_frame_interval, &fi);
 #else
 		v4l2_subdev_call(ctx->sensor, video, g_frame_interval, &fi);
