ae5a16ab01d010f88ece3088a2a5563268132869
Wen-jie Pan <Wen-jie.Pan@mediatek.com>
2025-02-05 08:27:31 +0800

    [ALPS09440596] camsv: remove BW * 5 WA
    [Description]
    1. Remove 5x camsv BW  WA
    
    [Test]
    FCT Test pass
    
    MTK-Commit-Id: ce651dbe963667a831c11b2cee2bb1eb4eacccd8
    
    CR-Id: ALPS09440596
    Signed-off-by: Wen-jie Pan <Wen-jie.Pan@mediatek.com>
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I8da5f2c73f057eaa1119d3fe123ca11163ea2936

--- camsys/isp8s/cam/mtk_cam-dvfs_qos.c
+++ camsys/isp8s/cam/mtk_cam-dvfs_qos.c
@@ -773,9 +773,9 @@ static int fill_sv_qos(struct mtk_cam_job *job,
 
 		if (i < SVTAG_IMG_END) {
 			avg_bw =
-				calc_bw(x_size * img_h, linet, sensor_h + sensor_vb) * 5;
+				calc_bw(x_size * img_h, linet, sensor_h + sensor_vb);
 			peak_bw =
-				calc_bw(x_size * img_h, linet, sensor_h) * 5;
+				calc_bw(x_size * img_h, linet, sensor_h);
 			if (ipifmt_is_raw_ufo(in->fmt.format)) {
 				/* compression ratio: 0.7x */
 				avg_bw = avg_bw * 7 / 10;
@@ -792,9 +792,9 @@ static int fill_sv_qos(struct mtk_cam_job *job,
 
 		} else {
 			avg_bw =
-				calc_bw(x_size * img_h, linet, sensor_h + sensor_vb) * 5;
+				calc_bw(x_size * img_h, linet, sensor_h + sensor_vb);
 			peak_bw =
-				calc_bw(x_size * img_h, linet, sensor_h) * 5;
+				calc_bw(x_size * img_h, linet, sensor_h);
 			/* camsv stash fixed at 5ostdl = 5mb */
 			if (avg_bw || peak_bw)
 				stash_peak_bw = stash_avg_bw = to_qos_icc(2097152);
