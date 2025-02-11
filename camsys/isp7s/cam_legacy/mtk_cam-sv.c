2c302cc9291112df798c385bfdae7722fafd2817
mtk32677 <vian-ya.shang@mediatek.com>
2025-02-11 13:43:27 +0800

    [ALPS09685649] isp7s coverity
    [Description]
    19216616:mf = NULL return value from get_sv_fmt
    [ALPS09686856]19216405:mf = NULL return value from get_sv_fmt.
    [Test]
    Build Pass
    
    MTK-Commit-Id: ff66c6adeaa36be056f0ef38118275779b6a1fdc
    
    CR-Id: ALPS09685649
    Feature: isp_camsv
    Signed-off-by: mtk32677 <Vian-ya.Shang@mediatek.com>
    Change-Id: Ideb784cd59820fd9ea7898113739cdca24773aa2

--- camsys/isp7s/cam_legacy/mtk_cam-sv.c
+++ camsys/isp7s/cam_legacy/mtk_cam-sv.c
@@ -302,6 +302,10 @@ static int mtk_camsv_call_set_fmt(struct v4l2_subdev *sd,
 
 	if (mtk_camsv_try_fmt(sd, fmt) == MTKCAM_IPI_IMG_FMT_UNKNOWN) {
 		mf = get_sv_fmt(pipe, state, fmt->pad, fmt->which);
+		if (!mf) {
+			dev_info(sd->v4l2_dev->dev, "sd:%s no format\n", sd->name);
+			return -EINVAL;
+		}
 		fmt->format = *mf;
 
 		dev_info(sv->cam_dev,
@@ -309,6 +313,10 @@ static int mtk_camsv_call_set_fmt(struct v4l2_subdev *sd,
 			sd->name, fmt->pad, mf->width, mf->height, mf->code, fmt->which);
 	} else {
 		mf = get_sv_fmt(pipe, state, fmt->pad, fmt->which);
+		if (!mf) {
+			dev_info(sd->v4l2_dev->dev, "sd:%s no format\n", sd->name);
+			return -EINVAL;
+		}
 		*mf = fmt->format;
 
 		dev_info(sv->cam_dev,
