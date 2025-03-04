ccc86f145b3c80a3248f81c457c3f67e452e5001
mtk28436 <larry.chung@mediatek.com>
2025-03-04 10:13:05 +0800

    [ALPS09802609] camsys:seninf err detect
    [Description]
    1. customerize err detection for fixing racing.
    2. sync patch from 9796454, 9156306
    
    [Test]
    UT pass/ local build pass/customer stress
    
    MTK-Commit-Id: 0f5584a3aaa521cf54253435157564da7410c795
    
    CR-Id: ALPS09802609
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: mtk28436 <larry.chung@mediatek.com>
    Change-Id: Id9c6f9a5b6b85c66b4f469f3284cae221054fe76

--- camsys/isp8/cam/mtk_csi_phy_3_0/mtk_cam-seninf-hw_phy_3_0.c
+++ camsys/isp8/cam/mtk_csi_phy_3_0/mtk_cam-seninf-hw_phy_3_0.c
@@ -5184,11 +5184,22 @@ static void dump_mipi_error_detect_info(struct seninf_core *core,
 	struct seninf_ctx *ctx_;
 	int i;
 
+	if (core->refcnt == 0) {
+		pr_info("[%s][ERROR] refcnt is = 0 already\n", __func__);
+		return;
+	}
+
 	for (i = 0; i < vsync_info->used_csi_port_num; i++) {
 		list_for_each_entry(ctx_, &core->list, list) {
-			if (vsync_info->ctx_port[i] == ctx_->port) {
-				calculate_mipi_error_cnt(core, ctx_, vsync_info->csi_irq_st[i]);
-				dump_current_mipi_error_cnt(core, ctx_, vsync_info);
+			if (ctx_->streaming && ctx_->power_status_flag) {
+				if (vsync_info->ctx_port[i] == ctx_->port) {
+					calculate_mipi_error_cnt(core, ctx_,
+						vsync_info->csi_irq_st[i]);
+					dump_current_mipi_error_cnt(core, ctx_, vsync_info);
+				}
+			} else {
+				pr_info("[%s][ERROR] check streaming/power_status_flag is 0?\n",
+					__func__);
 			}
 		}
 	}
