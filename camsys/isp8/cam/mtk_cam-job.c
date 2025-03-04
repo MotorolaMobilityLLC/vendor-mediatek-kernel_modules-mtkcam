4d56833b70da4ba74fbea1d324fe8d145179ca59
mtk14303 <Hsiencheng.Chang@mediatek.com>
2025-03-04 10:13:08 +0800

    [ALPS09786764] camsys: stream on
    [Description]
    camsys vf on before stream on seninf
    
    [Test]
    stress test
    
    MTK-Commit-Id: 3d8c8e498e5d76d33b118e733d64fddf32d4a56d
    
    Signed-off-by: mtk14303 <Hsiencheng.Chang@mediatek.com>
    CR-Id: ALPS09786764
    Feature: isp_camsv
    Change-Id: If4977de263d32326197c12eae2d72dfcab08dd01

--- camsys/isp8/cam/mtk_cam-job.c
+++ camsys/isp8/cam/mtk_cam-job.c
@@ -1275,10 +1275,6 @@ _stream_on(struct mtk_cam_job *job, bool on)
 	if (is_ois_compensation(job))
 		mtk_cam_tuning_init(&job->tuning_param);
 
-	/* TODO: separate seninf api to cammux setting and enable */
-	if (job->stream_on_seninf || job->raw_switch)
-		ctx_stream_on_seninf_sensor(job, pad_bitmask, raw_tg_idx);
-
 	if (job->raw_change && !job->seamless_switch) {
 		disable_seninf_cammux(job);
 		apply_cam_mux_switch(job);
@@ -1328,6 +1324,10 @@ _stream_on(struct mtk_cam_job *job, bool on)
 			}
 		}
 	}
+
+	/* TODO: separate seninf api to cammux setting and enable */
+	if (job->stream_on_seninf || job->raw_switch)
+		ctx_stream_on_seninf_sensor(job, pad_bitmask, raw_tg_idx);
 
 	return 0;
 }
