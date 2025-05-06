eba2225d177dead59d9483e350e41203e475b0b8
mtk32677 <vian-ya.shang@mediatek.com>
2025-05-06 15:45:31 +0800

    [ALPS09986468] add serror dbg log
    [Description]
    add adb cmd to enable serror dbg log
    [Test]
    Build Pass
    
    MTK-Commit-Id: 6d99b67cb6e22b917631b821a4247c188c502163
    
    CR-Id: ALPS09986468
    Feature: isp_camsv
    Signed-off-by: mtk32677 <Vian-ya.Shang@mediatek.com>
    Change-Id: I18e04d450f89f2ec449832a132bc9390cb12266e

--- camsys/isp8s/cam/mtk_cam-sv.c
+++ camsys/isp8s/cam/mtk_cam-sv.c
@@ -75,6 +75,10 @@ static int camsv_debug_dump_once;
 module_param(camsv_debug_dump_once, int, 0644);
 MODULE_PARM_DESC(camsv_debug_dump_once, "camsv debug dump once");
 
+static int serror_dbg_chk;
+module_param(serror_dbg_chk, int, 0644);
+MODULE_PARM_DESC(serror_dbg_chk, "camsv debug serror apb");
+
 #undef dev_dbg
 #define dev_dbg(dev, fmt, arg...)		\
 	do {					\
@@ -665,6 +669,7 @@ void sv_reset_by_camsys_top(struct mtk_camsv_device *sv_dev)
 {
 	int cq_dma_sw_ctl;
 	int ret;
+	unsigned int vcore_gal;
 
 	dev_info(sv_dev->dev, "%s camsv_id:%d\n", __func__, sv_dev->id);
 
@@ -689,10 +694,31 @@ void sv_reset_by_camsys_top(struct mtk_camsv_device *sv_dev)
 	}
 	/* debug only, rm soon */
 	writel(0, sv_dev->base_scq + REG_CAMSVCQTOP_SW_RST_CTL);
+	if (serror_dbg_chk) {
+		// vcore
+		vcore_gal = readl(sv_dev->cam->vcore_base + 0x10);
+		pr_info("vcore gal read: 0x%x", readl(sv_dev->cam->vcore_base + 0x10));
+		vcore_gal &= ~(0x2000);
+		vcore_gal |= (1 << 13);
+		writel(vcore_gal, sv_dev->cam->vcore_base + 0x10);
+		pr_info("vcore gal write: 0x%x", readl(sv_dev->cam->vcore_base + 0x10));
+		// main
+		writel(0xDEADBEEF, sv_dev->cam->base + 0x10);
+		pr_info("main sram_del: 0x%x", readl(sv_dev->cam->base + 0x10));
+		// mraw
+		writel(0xDEADBEEF, sv_dev->top + 0x10);
+		pr_info("mraw sram_del: 0x%x", readl(sv_dev->top + 0x10));
+	}
 
 	writel(0, sv_dev->top + REG_CAM_MAIN_SW_RST_1);
+	if (serror_dbg_chk)
+		pr_info("reset_dbg_step 1");
 	writel(3 << ((sv_dev->id) * 2 + 4), sv_dev->top + REG_CAM_MAIN_SW_RST_1);
+	if (serror_dbg_chk)
+		pr_info("reset_dbg_step 2");
 	writel(0, sv_dev->top + REG_CAM_MAIN_SW_RST_1);
+	if (serror_dbg_chk)
+		pr_info("reset_dbg_step 3");
 	wmb(); /* make sure committed */
 
 RESET_FAILURE:
