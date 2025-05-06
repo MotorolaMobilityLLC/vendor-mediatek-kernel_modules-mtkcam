d351520f69460e1ff1b2fe7efee2a6c2edf03d4d
Ellie Chiu <ds_ellie.chiu@mediatek.com>
2025-05-06 15:44:38 +0800

    [ALPS09919247] cam: JN1 quick streaming on/off
    [Description]
    1. add AE ctrl locker
    2. add check streaming status
    
    [Test]
    ITS test / MM PDAS
    
    MTK-Commit-Id: 4a140ae6dc757a325829aa66d8d78b99a13fada4
    
    Signed-off-by: Ellie Chiu <ds_ellie.chiu@mediatek.com>
    CR-Id: ALPS09919247
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I6229119aa7e100d997ce3a0b09962d90a516fe3d

--- imgsensor/src-isp8s/common/s5kjn1_mipi_raw/s5kjn1mipiraw_Sensor.c
+++ imgsensor/src-isp8s/common/s5kjn1_mipi_raw/s5kjn1mipiraw_Sensor.c
@@ -27,6 +27,8 @@ static void set_group_hold(void *arg, u8 en);
 static u16 get_gain2reg(u32 gain);
 static int s5kjn1_set_test_pattern(struct subdrv_ctx *ctx, u8 *para, u32 *len);
 static int s5kjn1_set_test_pattern_data(struct subdrv_ctx *ctx, u8 *para, u32 *len);
+static int s5kjn1_set_ctrl_locker(struct subdrv_ctx *ctx, u32 cid, bool *is_lock);
+static int s5kjn1_chk_streaming_st(void *arg);
 
 /* STRUCT */
 
@@ -40,15 +42,7 @@ static struct subdrv_static_ctx_ext_ops static_ext_ops = {
 	.g_temp = get_sensor_temperature,
 	.g_gain2reg = get_gain2reg,
 	.s_gph = set_group_hold,
-
-	/*when setting a long exp and no autoextend FL, extend frame length related member*/
-	/*
-	 * .reg_addr_frame_length_lshift = 0x0702,
-	 * .fll_lshift_max = 7,
-	 * .cit_lshift_max = 7, //default=7 max=11
-	 * .stagger_rg_orger = IMGSENSOR_STAGGER_RG_SE_FIRST,
-	 * .stagger_fl_type = IMGSENSOR_STAGGER_FL_MANUAL,
-	 */
+	.chk_streaming_st = s5kjn1_chk_streaming_st,
 	.list = feature_control_list,
 	.list_len = ARRAY_SIZE(feature_control_list),
 #ifdef S5KJN1_ISF_DBG
@@ -69,6 +63,7 @@ static struct subdrv_ops ops = {
 	.get_temp = common_get_temp,
 	.get_csi_param = common_get_csi_param,
 	.update_sof_cnt = common_update_sof_cnt,
+	.set_ctrl_locker = s5kjn1_set_ctrl_locker,
 };
 
 
@@ -146,4 +141,57 @@ static int s5kjn1_set_test_pattern_data(struct subdrv_ctx *ctx, u8 *para, u32 *l
 
 	return 0;
 }
+static int s5kjn1_set_ctrl_locker(struct subdrv_ctx *ctx,
+		u32 cid, bool *is_lock)
+{
+	bool lock_set_ctrl = false;
 
+	if (unlikely(is_lock == NULL)) {
+		pr_info("[%s][ERROR] is_lock %p is NULL\n", __func__, is_lock);
+		return -EINVAL;
+	}
+
+	switch (cid) {
+	case V4L2_CID_MTK_STAGGER_AE_CTRL:
+	case V4L2_CID_MTK_MAX_FPS:
+		if ((ctx->sof_no == 0) && (ctx->is_streaming)) {
+			lock_set_ctrl = true;
+			DRV_LOG(ctx,
+				"[%s] Target lock cid(%u) lock_set_ctrl(%d), sof_no(%d) is_streaming(%d)\n",
+				__func__,
+				cid,
+				lock_set_ctrl,
+				ctx->sof_no,
+				ctx->is_streaming);
+		}
+		break;
+	default:
+		break;
+	}
+
+	*is_lock = lock_set_ctrl;
+	return ERROR_NONE;
+} /* s5kjn1_set_ctrl_locker */
+
+static int s5kjn1_chk_streaming_st(void *arg)
+{
+	struct subdrv_ctx *ctx = (struct subdrv_ctx *)arg;
+	u64 cur_time = 0;
+	u64 start_time = ctx->stream_ctrl_start_time;
+	u32 cur_fc = 0xff;
+	u64 timeout = 1000000000/(u64)ctx->current_fps*10;
+	int ret = ERROR_NONE;
+
+	subdrv_ixc_wr_u16(ctx, 0x6028, 0x4000);
+	subdrv_ixc_wr_u16(ctx, 0x602A, 0x0005);
+	while (1) {
+		cur_time = ktime_get_boottime_ns();
+		cur_fc =  subdrv_ixc_rd_u8(ctx, 0x0005);
+		if ((cur_fc > 0 && cur_fc != 0xff) || (cur_time - start_time > timeout))
+			break;
+		DRV_LOG(ctx, "current_fps: %d, timeout: %lld, cur_fc: %d, cur_time: %lld, start_time: %lld\n",
+			ctx->current_fps, timeout, cur_fc, cur_time, start_time);
+		mdelay(1);
+	}
+	return ret;
+}
