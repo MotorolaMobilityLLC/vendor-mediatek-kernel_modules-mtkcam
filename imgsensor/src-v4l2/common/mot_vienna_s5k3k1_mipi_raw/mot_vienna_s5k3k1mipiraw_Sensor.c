4fc4405351afd04f4a0d38de8f6cec8a065f0f8c
qiaopf1 <qiaopf1@motorola.com>
2025-06-18 11:30:11 +0800

    paris: modify sensor power sequence
    details：
    since ldo is replaced by aw37004, dvdd delay needs to be increased
    
    Change-Id: I8b33dfcc007ec9c345a1c99424c671850bce28f8
    Signed-off-by: qiaopf1 <qiaopf1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3434578
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 2fd9f5cfa1b468db8462f23c9e50413a12beb120)
    Reviewed-on: https://gerrit.mot.com/3436397
    Submit-Approved: Sen Ling <lingsen2@motorola.com>
    Reviewed-by: Hao Li <lihao24@motorola.com>

--- imgsensor/src-v4l2/common/mot_vienna_s5k3k1_mipi_raw/mot_vienna_s5k3k1mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/mot_vienna_s5k3k1_mipi_raw/mot_vienna_s5k3k1mipiraw_Sensor.c
@@ -499,7 +499,7 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
 	{HW_ID_DOVDD, 1800000, 1},
 	{HW_ID_AVDD, 2800000, 6}, // pmic_ldo for avdd
-	{HW_ID_DVDD, 1050000, 6}, // pmic_ldo for dvdd
+	{HW_ID_DVDD, 1050000, 8}, // pmic_ldo for dvdd
 	{HW_ID_MCLK_DRIVING_CURRENT, 8, 0},
 	{HW_ID_RST, 1, 4}
 };
