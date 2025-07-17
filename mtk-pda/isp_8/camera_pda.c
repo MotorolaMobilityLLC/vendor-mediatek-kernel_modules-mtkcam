67bc43ffc08b7e62bd6c13944cc449f3198167f9
po-hao.huang <po-hao.huang@mediatek.com>
2025-07-17 09:47:42 +0800

    [ALPS10127160] pda: avoid UAF issue
    [Description]
    security issue, use after free
    
    [Test]
    PDA UT/IT pass, PDAS, MTBF
    
    MTK-Commit-Id: 2f65222c8eb13c4e68080c86668ec2cef3d0d508
    
    Change-Id: Ic462644fd7ce724916564ce003757bf940cce635
    CR-Id: ALPS10127160
    Feature: AF(Auto Focus)
    Signed-off-by: po-hao.huang <po-hao.huang@mediatek.com>

--- mtk-pda/isp_8/camera_pda.c
+++ mtk-pda/isp_8/camera_pda.c
@@ -1972,6 +1972,8 @@ static void free_alldmabufferandfdarraytable(void)
 	if (pda_log_dbg_en == 1)
 		LOG_INF("+\n");
 
+	mutex_lock(&pda_mutex);
+
 	// ------------- free image related (include blending) -------------------
 	for (i = 0; i < ARRAY_SIZE(fd_l_img_rec); ++i) {
 		if (pda_log_dbg_en == 1)
@@ -1983,8 +1985,9 @@ static void free_alldmabufferandfdarraytable(void)
 				LOG_INF("unmap img done\n");
 			break;
 		}
-		fd_l_img_rec[i] = 0;
+
 		pda_put_dma_buffer(&g_image_mmu[i]);
+		fd_l_img_rec[i] = 0;
 		g_Address_LI[i] = 0;
 		g_Address_RI[i] = 0;
 	}
@@ -2000,8 +2003,9 @@ static void free_alldmabufferandfdarraytable(void)
 				LOG_INF("unmap tbl done\n");
 			break;
 		}
-		fd_l_tbl_rec[i] = 0;
+
 		pda_put_dma_buffer(&g_table_mmu[i]);
+		fd_l_tbl_rec[i] = 0;
 		g_Address_LT[i] = 0;
 		g_Address_RT[i] = 0;
 	}
@@ -2017,8 +2021,9 @@ static void free_alldmabufferandfdarraytable(void)
 				LOG_INF("unmap output done\n");
 			break;
 		}
-		fd_out_rec[i] = 0;
+
 		pda_put_dma_buffer(&g_output_mmu[i]);
+		fd_out_rec[i] = 0;
 		g_OutputBufferAddr[i] = 0;
 	}
 
@@ -2026,6 +2031,8 @@ static void free_alldmabufferandfdarraytable(void)
 	g_ring_img_idx = 0;
 	g_ring_tbl_idx = 0;
 	g_ring_out_idx = 0;
+
+	mutex_unlock(&pda_mutex);
 
 	if (pda_log_dbg_en == 1)
 		LOG_INF("-\n");
