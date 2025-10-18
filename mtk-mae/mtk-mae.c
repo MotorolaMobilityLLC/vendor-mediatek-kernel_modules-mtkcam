8e015105adf2b6e0cf4515dc18a3f1af6c11335f
Ming Teng <mingteng.wu@mediatek.com>
2025-10-19 00:54:33 +0800

    [ALPS10432500] mae: Fix OOB write vulnerability
    [Description]
    Resolved an out-of-bounds
    
    [Test]
    Build Pass
    
    MTK-Commit-Id: 190edaf410a3cc229f3db069c03169c3680ee777
    
    CR-Id: ALPS10432500
    Feature: Others
    Change-Id: I4d7d23677c8c1b6874845e9f55bbfb5d257b5df9
    Signed-off-by: Ming Teng <mingteng.wu@mediatek.com>
    (cherry-pick from 7512512eb5a2f48d65e6f14b85b4c6ee90e48a67)

--- mtk-mae/mtk-mae.c
+++ mtk-mae/mtk-mae.c
@@ -948,6 +948,8 @@ static int mtk_mae_hw_connect(struct mtk_mae_dev *mae_dev)
 		/* register slc debug */
 		if (mae_slc_dbg_en) {
 			mae_gid = -1;
+			if (mae_slbc_gid_data)
+				vfree(mae_slbc_gid_data);
 			mae_slbc_gid_data = vzalloc(sizeof(struct slbc_gid_data));
 			mae_slbc_gid_data->sign = SLC_DATA_MAGIC;
 			ret = slbc_gid_request(ID_MAE, &mae_gid, mae_slbc_gid_data);
@@ -1029,8 +1031,10 @@ static void mtk_mae_umap_detach(struct mtk_mae_dev *mae_dev,
 		info->is_attach = false;
 	}
 
-	if (!IS_ERR(info->dmabuf) && info->dmabuf)
+	if (!IS_ERR(info->dmabuf) && info->dmabuf){
 		dma_buf_put(info->dmabuf);
+		info->dmabuf = NULL;
+	}
 
 	info->kva = 0;
 	info->pa = 0;
@@ -1124,6 +1128,7 @@ static void mtk_mae_hw_disconnect(struct mtk_mae_dev *mae_dev)
 				dev_info(mae_dev->dev, "slc release fail");
 
 			vfree(mae_slbc_gid_data);
+			mae_slbc_gid_data = NULL;
 		}
 	#endif
 
@@ -1599,6 +1604,12 @@ int mtk_mae_vidioc_qbuf(struct file *file, void *priv,
 
 	map_table = mae_dev->map_table;
 
+	if (idx >= REQUEST_BUFFER_NUM) {
+		mae_dev_info(mae_dev->dev, "%s, invalid buffer index: %u\n",
+			__func__, buf->index);
+		return -EINVAL;
+	}
+
 	if (buf->length < MAX_PLANE) {
 		mae_dev_info(mae_dev->dev, "%s, buf length is too small (%d/%d)\n",
 			__func__, buf->length, MAX_PLANE);
@@ -1815,7 +1826,6 @@ int mtk_mae_vidioc_qbuf(struct file *file, void *priv,
 			}
 		}
 	}
-
 	// get pa of image
 	mtk_mae_umap_detach(mae_dev, &map_table->image_dmabuf_info[idx]);
 	ret = mtk_mae_set_dmabuf_info(mae_dev,
