27303a0c98bcbaccb1969f7cb990f08c883e9d21
Shi Zuntao <shizt1@motorola.com>
2026-03-03 14:24:24 +0800

    merc: drv: [PATCH] [ALPS10905200] face race customization
    [Description]
     The terrain needs to support Indian ethnicity identification.
    
    [Test]
     Indians can be identified.
    
    Change-Id: If0b9e3d5bf85e74d613b526ce4b552c538d98687
    Signed-off-by: Shi Zuntao <shizt1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3788090
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Dong Han <handong9@motorola.com>
    Reviewed-by: <daizm1@motorola.com>
    Submit-Approved: Jira Key

--- mtk-mae/isp8/mtk-mae-isp8.c
+++ mtk-mae/isp8/mtk-mae-isp8.c
@@ -264,17 +264,17 @@ struct mae_priv_data priv_data_isp8_mt6899 = {
 		}
 	},
 	.fac_v1_config_info = {
-		.size = 428,
-		.rotate_offset = 3184,
-		.rotate_size = 433,
+		.size = 433,
+		.rotate_offset = 3193,
+		.rotate_size = 439,
 	},
 	.fac_v1_coef_info = {
 		.size = 1599,
 	},
 	.fd_v1_fpn_config_info = {
-		.size = 448,
-		.rotate_offset = 6278,
-		.rotate_size = 453,
+		.size = 453,
+		.rotate_offset = 6296,
+		.rotate_size = 458,
 	},
 	.fd_v1_fpn_coef_info = {
 		.size = 2306,
